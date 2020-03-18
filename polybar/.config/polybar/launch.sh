3255539eb1f37275080b4dd4359718e8b0c59291
Gaetan Lepage <gaetan.lepage@grenoble-inp.org>
2020-03-18 20:30:46 +0100

    added scripts

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -4,10 +4,11 @@
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -x polybar >/dev/null; do sleep 1; done
 
-for m in $(polybar --list-monitors | cut -d":" -f1); do
-	WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') MONITOR=$m polybar --reload -l info mainbar-i3 &
-done
+#for m in $(polybar --list-monitors | cut -d":" -f1); do
+	#WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') MONITOR=$m polybar --reload -l info mainbar-i3 &
+#done
+for i in $(polybar -m | awk -F: '{print $1}'); do MONITOR=$i polybar default & done
 
 echo "Bars launched..."
