add56162790dbf27b3cba42ce0049429efffaa56
Gaetan Lepage <gaetan.lepage@inria.fr>
2021-01-15 18:57:28 +0100

    update polybar click

---
+++ config/polybar/update.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+type pacman >/dev/null 2>&1 || exit
+
+if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
+    updates_arch=0
+fi
+
+if ! updates_aur=$(yay -Qum 2> /dev/null | wc -l); then
+    updates_aur=0
+fi
+
+updates=$(("$updates_arch" + "$updates_aur"))
+
+if [ "$updates" -gt 0 ]; then
+    $TERMINAL --class update yay -Syu --noconfirm &
+    sleep 1
+    i3-msg "scratchpad show"
+else
+    echo "No updates"
+    notify-send "No updates available"
+fi
