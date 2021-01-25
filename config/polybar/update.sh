a7cca2f99128752c72fb4ebd57c995cc61aa222c
Gaetan Lepage <gaetan.lepage@inria.fr>
2021-01-25 10:01:52 +0100

    [Polybar] switched from yay to paru + update script

--- config/polybar/update.sh
+++ config/polybar/update.sh
@@ -2,19 +2,37 @@
 
 type pacman >/dev/null 2>&1 || exit
 
-if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
-    updates_arch=0
-fi
+check_updates() {
+    if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
+        updates_arch=0
+    fi
 
-if ! updates_aur=$(yay -Qum 2> /dev/null | wc -l); then
-    updates_aur=0
-fi
+    if ! updates_aur=$(paru -Qum 2> /dev/null | wc -l); then
+        updates_aur=0
+    fi
 
-updates=$(("$updates_arch" + "$updates_aur"))
+    total_updates=$(("$updates_arch" + "$updates_aur"))
+}
 
-if [ "$updates" -gt 0 ]; then
-    $TERMINAL --class update yay -Syu --noconfirm &
-else
-    echo "No updates"
-    notify-send "No updates available"
-fi
+# Fetch updates
+check_updates
+
+case $1 in
+    --check-updates)
+        echo " $updates_arch |  $updates_aur"
+        ;;
+
+    --do-updates)
+        if [ "$total_updates" -gt 0 ]; then
+            COMMAND="paru -Syu --noconfirm"
+            $TERMINAL --class update $COMMAND &
+        else
+            echo "No updates"
+            notify-send "  No updates available"
+        fi
+        ;;
+
+    *)
+        notify-send "wrong argument"
+        ;;
+esac
