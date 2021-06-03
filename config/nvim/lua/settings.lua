a7297e2f8536c3cfd5741a80e7525e86ef50b571
Gaetan Lepage <gaetan.lepage@inria.fr>
2021-06-03 10:40:12 +0200

    repaired vim sets

--- config/nvim/lua/settings.lua
+++ config/nvim/lua/settings.lua
@@ -29,15 +29,13 @@ utils.create_augroup('Highlights',
 vim.cmd[[ autocmd BufWrite * %s/\s\+$//e ]]
 
 local o = vim.o
-local wo = vim.wo
-local bo = vim.bo
 
 
 o.updatetime = 300          -- Faster completion
 
 -- Line numbers
-wo.relativenumber = true    -- Relative line numbers
-wo.number = true            -- display the absolute line number of the current line
+o.relativenumber = true     -- Relative line numbers
+o.number = true             -- display the absolute line number of the current line
 o.hidden = true             -- keep closed buffer open in the background
 o.clipboard = "unnamedplus" -- Use system clipboard
 o.mouse = "a"               -- Enable mouse control
@@ -48,41 +46,36 @@ vim.cmd [[
     set noswapfile
     set modeline
 ]]
--- bo.swapfile=false           -- Disable the swap file
--- bo.modeline=true            -- Tags such as 'vim:ft=sh'
+-- o.swapfile=false           -- Disable the swap file
+-- o.modeline=true            -- Tags such as 'vim:ft=sh'
 o.modelines = 100           -- Sets the type of modelines
 o.undofile = true           -- Automatically save and restore undo history
 o.incsearch = true          -- Incremental search: show match for partly typed search command
-o.smartcase = true          -- Override the 'ignorecase' option if the search pattern contains upper case characters
+o.smartcase = true          -- Override the 'ignorecase' option if the search
+                            -- pattern contains upper case characters
 o.scrolloff = 8             -- number of screen lines to show around the cursor
 --set cursorline            -- Highlight the screen line of the cursor
 --set cursorcolumn          -- Highlight the screen column of the cursor
-wo.signcolumn = "yes"       -- whether to show the signcolumn
-wo.colorcolumn = "100"      -- columns to highlight
+o.signcolumn = "yes"        -- whether to show the signcolumn
+o.colorcolumn = "100"       -- columns to highlight
 o.laststatus = 2            -- 0, 1 or 2; when to use a status line for the last window
 o.encoding = "utf-8"        -- encoding used to print the PostScript file for :hardcopy
 o.termguicolors = true      -- Enables 24-bit RGB color in the |TUI|
-wo.spell = false            -- highlight spelling mistakes (local to window)
+o.spell = false             -- highlight spelling mistakes (local to window)
 --syntax on                 -- Enable syntax highlighting
 
 -- Set completeopt to have a better completion experience
-vim.o.completeopt = "menuone,noselect"
+o.completeopt = "menuone,noselect"
 
 -- Tab options
-vim.cmd [[
-    set tabstop=4
-    set shiftwidth=4
-    set softtabstop=0
-    set expandtab
-    set smartindent
-]]
--- TODO set this when teej will have fixed the problem on neovim (https://github.com/neovim/neovim/issues/12978)
---bo.tabstop=4                -- number of spaces a <Tab> in the text stands for (local to buffer)
---bo.shiftwidth=4             -- number of spaces used for each step of (auto)indent (local to buffer)
---bo.softtabstop=0            -- if non-zero, number of spaces to insert for a <Tab> (local to buffer)
---bo.expandtab=true           -- expand <Tab> to spaces in Insert mode (local to buffer)
---bo.smartindent=true         -- do clever autoindenting
+o.tabstop = 4          -- number of spaces a <Tab> in the text stands for (local to buffer)
+o.shiftwidth = 4       -- number of spaces used for each step of (auto)indent (local to buffer)
+o.softtabstop = 0      -- if non-zero, number of spaces to insert for a <Tab> (local to buffer)
+o.expandtab = true     -- expand <Tab> to spaces in Insert mode (local to buffer)
+o.smartindent = true   -- do clever autoindenting
+
+o.textwidth = 100
 
 -- Folding
-wo.foldmethod = "indent" 	  -- Set folding type to indent
-wo.foldlevel = 99             -- Folds with a level higher than this number will be closed
+o.foldmethod = "indent" 	  -- Set folding type to indent
+o.foldlevel = 99             -- Folds with a level higher than this number will be closed
