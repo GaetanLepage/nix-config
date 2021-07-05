6bb1681bb9378f246bf0ca3f334c8296801596d8
Gaetan Lepage <gaetan.lepage@inria.fr>
2021-07-05 09:33:47 +0200

    updated telescope config

--- config/nvim/lua/nv-telescope/init.lua
+++ config/nvim/lua/nv-telescope/init.lua
@@ -7,14 +7,13 @@ require('nv-telescope.bindings')
 require('telescope').setup {
     defaults = {
         vimgrep_arguments = {
-	    'rg',
-	    '--no-heading',
-	    '--with-filename',
-	    '--line-number',
-	    '--column',
-	    '--smart-case'
+	        'rg',
+	        '--no-heading',
+	        '--with-filename',
+	        '--line-number',
+	        '--column',
+	        '--smart-case'
     	},
-        prompt_position = "bottom",
         prompt_prefix = "> ",
         selection_caret = "> ",
         entry_prefix = "  ",
@@ -22,19 +21,15 @@ require('telescope').setup {
         selection_strategy = "reset",
         sorting_strategy = "descending",
         layout_strategy = "horizontal",
-        layout_defaults = {
-	    horizontal = {mirror = false},
-	    vertical = {mirror = false}
+        layout_config = {
+	        horizontal = {mirror = false},
+	        vertical = {mirror = false}
     	},
         file_sorter = require'telescope.sorters'.get_fuzzy_file,
         file_ignore_patterns = {},
         generic_sorter = require'telescope.sorters'.get_generic_fuzzy_sorter,
         shorten_path = true,
         winblend = 0,
-        width = 0.75,
-        preview_cutoff = 120,
-        results_height = 1,
-        results_width = 0.8,
         border = {},
         borderchars = {'─', '│', '─', '│', '╭', '╮', '╯', '╰'},
         color_devicons = true,
@@ -46,29 +41,5 @@ require('telescope').setup {
 
         -- Developer configurations: Not meant for general override
         buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker,
---         mappings = {
---             i = {
---                 ["<C-j>"] = actions.move_selection_next,
---                 ["<C-k>"] = actions.move_selection_previous,
---                 -- To disable a keymap, put [map] = false
---                 -- So, to not map "<C-n>", just put
---                 -- ["<c-x>"] = false,
---                 ["<esc>"] = actions.close,
---
---                 -- Otherwise, just set the mapping to the function that you want it to be.
---                 -- ["<C-i>"] = actions.select_horizontal,
---
---                 -- Add up multiple actions
---                 ["<CR>"] = actions.select_default + actions.center
---
---                 -- You can perform as many actions in a row as you like
---                 -- ["<CR>"] = actions.select_default + actions.center + my_cool_custom_action,
---             },
---             n = {
---                 ["<C-j>"] = actions.move_selection_next,
---                 ["<C-k>"] = actions.move_selection_previous
---                 -- ["<C-i>"] = my_cool_custom_action,
---             }
---         }
     }
 }
