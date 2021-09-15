40c4e7225b060b2286f9fe585b154bf17bc59b5e
Gaetan Lepage <gaetan.lepage@inria.fr>
2021-09-15 13:54:54 +0200

    [vim] do not run markdown preview by default

--- config/nvim/lua/plugins/markdown-preview.lua
+++ config/nvim/lua/plugins/markdown-preview.lua
@@ -2,7 +2,7 @@ vim.api.nvim_set_keymap('n', '<leader>m', ':MarkdownPreview<CR>', {noremap=true,
 -- vim.g.mkdp_browser = 'qutebrowser'
 vim.g.mkdp_browser = 'firefox'
 
-vim.g.mkdp_auto_start = 1
+vim.g.mkdp_auto_start = 0
 vim.g.mkdp_auto_stop = 1
 
 -- vim.g.mkdp_echo_preview_url = 0
