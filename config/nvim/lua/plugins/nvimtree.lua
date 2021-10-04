19cc0821098f0833b8a3a6877c46f1c139a5cb81
Gaetan Lepage <gaetan.lepage@inria.fr>
2021-10-05 00:56:39 +0200

    [vim] nvimtree option

--- config/nvim/lua/plugins/nvimtree.lua
+++ config/nvim/lua/plugins/nvimtree.lua
@@ -22,7 +22,7 @@ require'nvim-tree'.setup {
     -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
     update_focused_file = {
         -- enables the feature
-        enable      = false,
+        enable      = true,
         -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
         -- only relevant when `update_focused_file.enable` is true
         update_cwd  = false,
