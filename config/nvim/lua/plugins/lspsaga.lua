ee3fbc851047ae138d14494c32b1b4aaf4daf04a
Gaetan Lepage <gaetan.lepage@inria.fr>
2021-10-17 19:05:39 +0200

    [vim] lspsaga config (+ disabled codeactions because they were erroring)

--- config/nvim/lua/plugins/lspsaga.lua
+++ config/nvim/lua/plugins/lspsaga.lua
@@ -1,46 +1,50 @@
-local saga = require 'lspsaga'
+require 'lspsaga'.setup {
+    debug = false,
+    use_saga_diagnostic_sign = true,
 
--- use_saga_diagnostic_sign = true
--- error_sign = '',
--- warn_sign = '',
--- hint_sign = '',
--- infor_sign = '',
--- dianostic_header_icon = '   ',
--- code_action_icon = ' ',
--- code_action_prompt = {
---   enable = true,
---   sign = true,
---   sign_priority = 20,
---   virtual_text = true,
--- },
--- finder_definition_icon = '  ',
--- finder_reference_icon = '  ',
--- max_preview_lines = 10, -- preview lines of lsp_finder and definition preview
--- finder_action_keys = {
---   open = 'o', vsplit = 's',split = 'i',quit = 'q',scroll_down = '<C-f>', scroll_up = '<C-b>' -- quit can be a table
--- },
--- code_action_keys = {
---   quit = 'q',exec = '<CR>'
--- },
--- rename_action_keys = {
---   quit = '<C-c>',exec = '<CR>'  -- quit can be a table
--- },
--- definition_preview_icon = '  '
--- "single" "double" "round" "plus"
--- border_style = "single"
--- rename_prompt_prefix = '➤',
--- if you don't use nvim-lspconfig you must pass your server name and
--- the related filetypes into this table
--- like server_filetype_map = {metals = {'sbt', 'scala'}}
--- server_filetype_map = {}
+    -- diagnostic sign
+    error_sign = "",
+    warn_sign = "",
+    hint_sign = "",
+    infor_sign = "",
+    dianostic_header_icon = "   ",
 
--- saga.init_lsp_saga {
---   your custom option here
--- }
+    -- code action title icon
+    code_action_icon = " ",
+    code_action_prompt = {
+        enable = false,
+        sign = true,
+        sign_priority = 40,
+        virtual_text = true,
+    },
 
---use default config
-saga.init_lsp_saga()
+    finder_definition_icon = "  ",
+    finder_reference_icon = "  ",
+    max_preview_lines = 10,
+    finder_action_keys = {
+        open = "o",
+        vsplit = "s",
+        split = "i",
+        quit = "q",
+        scroll_down = "<C-f>",
+        scroll_up = "<C-b>",
+    },
 
+    code_action_keys = {
+        quit = "q",
+        exec = "<CR>",
+    },
+
+    rename_action_keys = {
+        quit = "<C-c>",
+        exec = "<CR>",
+    },
+
+    definition_preview_icon = "  ",
+    border_style = "single",
+    rename_prompt_prefix = "➤",
+    server_filetype_map = {},
+}
 
 ------------------
 -- Key mappings --
