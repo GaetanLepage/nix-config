0e7f223c1c77701eb350f0183b0c8f589b290163
Gaetan Lepage <gaetan.lepage@inria.fr>
2021-10-31 16:35:27 +0100

    [vim] packer formatting

--- config/nvim/lua/plugins/packer.lua
+++ config/nvim/lua/plugins/packer.lua
@@ -45,7 +45,8 @@ use {'nvim-treesitter/nvim-treesitter', -- TreeSitter (for syntax highlighting a
 
 -- Icons
 use 'kyazdani42/nvim-web-devicons'
-use 'ryanoasis/vim-devicons' 			-- A lua fork of vim-devicons. This plugin provides the same icons as well as colors for each icon.
+use 'ryanoasis/vim-devicons'            -- A lua fork of vim-devicons. This plugin provides the same
+                                        -- icons as well as colors for each icon.
 
 -- Status Line and Bufferline
 use 'hoob3rt/lualine.nvim'              -- The status line
@@ -61,18 +62,21 @@ use 'gruvbox-community/gruvbox'         -- Colorcheme
 use 'norcalli/nvim-colorizer.lua'       -- Preview colors directly from files
 
 -- Git
-use 'tpope/vim-fugitive' 				-- Git Fugitive
+use 'tpope/vim-fugitive'                -- Git Fugitive
 use 'airblade/vim-gitgutter'            -- Show a git diff in the sign column.
-use 'TimUntersberger/neogit' 			-- A work-in-progress Magit clone for Neovim that is geared toward the Vim philosophy.
+use 'TimUntersberger/neogit'            -- A work-in-progress Magit clone for Neovim that is geared
+                                        --  toward the Vim philosophy.
 
 -- General Plugins
-use 'windwp/nvim-autopairs' 			-- A minimalist autopairs for Neovim written in Lua
-use 'kevinhwang91/nvim-bqf'             -- make Neovim's quickfix window better
+use 'windwp/nvim-autopairs'             -- A minimalist autopairs for Neovim written in Lua
+use 'kevinhwang91/nvim-bqf'             -- Make Neovim's quickfix window better
 use 'kevinhwang91/rnvimr'               -- Allows you to use Ranger in a floating window
 use 'mhinz/vim-startify'                -- Start screen
-use 'moll/vim-bbye'                     -- Delete buffers (close files) without closing your windows or messing up the layout
+use 'moll/vim-bbye'                     -- Delete buffers (close files) without closing your windows
+                                        --  or messing up the layout
 use 'voldikss/vim-floaterm'             -- A floating terminal window in neovim
-use 'majutsushi/tagbar'                 -- Easy way to browse the tags of the current file and get an overview of its structure
+use 'majutsushi/tagbar'                 -- Easy way to browse the tags of the current file and get
+                                        --  an overview of its structure
 use 'terrortylor/nvim-comment'          -- Offer key mapping to comment code
 use 'bfredl/nvim-miniyank'
 
