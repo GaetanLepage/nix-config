38c37293d5b2edab12983db864c0cfd13f965703
Gaetan Lepage <gaetan.lepage@inria.fr>
2021-11-17 14:50:10 +0100

    [vim] repairing tabnine config

--- config/nvim/lua/plugins/tabnine.lua
+++ config/nvim/lua/plugins/tabnine.lua
@@ -1,7 +1,7 @@
-require 'cmp_tabnine.config'.setup{
+require 'cmp_tabnine.config':setup{
     max_lines = 1000,
     max_num_results = 20,
     sort = true,
 	run_on_every_keystroke = true,
-	snippet_placeholder = '..',
+	snippet_placeholder = '..'
 }
