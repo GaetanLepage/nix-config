2cf9f7c0dcd1084e1ad0282fa34c70d47e77b87b
Gaetan Lepage <gaetan.lepage@inria.fr>
2021-12-11 09:32:49 +0100

    [vim] config refactoring for plugin 'comment'

--- config/nvim/lua/plugins/comment.lua
+++ config/nvim/lua/plugins/comment.lua
@@ -1,8 +1,5 @@
 require('nvim_comment').setup()
 
 local utils = require 'utils'
-local nmap = utils.nmap
-local vmap = utils.vmap
-
-nmap('<C-b>', ':CommentToggle<CR>')
-vmap('<C-b>', ':CommentToggle<CR>')
+utils.nmap('<C-b>', ':CommentToggle<CR>')
+utils.vmap('<C-b>', ':CommentToggle<CR>')
