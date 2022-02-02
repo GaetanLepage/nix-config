5a465a8268d5309203a712b43e72eedb6a7f12b3
Gaetan Lepage <gaetan.lepage@inria.fr>
2022-02-02 14:39:06 +0100

    [vim] keybinding utils: silent = true

--- config/nvim/lua/utils.lua
+++ config/nvim/lua/utils.lua
@@ -1,23 +1,32 @@
 local M = {}
 
+-- function map(mode, key, mode)
+--     vim.keymap.set()
+-- end
+
 function M.nmap(key, action)
-    vim.keymap.set('n', key, action)
+    vim.keymap.set('n', key, action,
+                   { silent = true })
 end
 
 function M.vmap(key, action)
-    vim.keymap.set('v', key, action)
+    vim.keymap.set('v', key, action,
+                   { silent = true })
 end
 
 function M.xmap(key, action)
-    vim.keymap.set('x', key, action)
+    vim.keymap.set('x', key, action,
+                   { silent = true })
 end
 
 function M.imap(key, action)
-    vim.keymap.set('i', key, action)
+    vim.keymap.set('i', key, action,
+                   { silent = true })
 end
 
 function M.smap(key, action)
-    vim.keymap.set('s', key, action)
+    vim.keymap.set('s', key, action,
+                   { silent = true })
 end
 
 local cmd = vim.cmd
