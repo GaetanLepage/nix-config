ffff51dedb726992bb79f45c714823a3eecb46d3
Gaetan Lepage <gaetan.lepage@inria.fr>
2022-03-16 10:17:37 +0100

    [vim] fix comment string for nix files

--- config/nvim/lua/plugins/comment.lua
+++ config/nvim/lua/plugins/comment.lua
@@ -1,5 +1,23 @@
-require('nvim_comment').setup()
+require 'nvim_comment'.setup()
 
 local utils = require 'utils'
 utils.nmap('<C-b>', ':CommentToggle<CR>')
 utils.vmap('<C-b>', ':CommentToggle<CR>')
+
+-- Manually set some comment strings
+local commentstring_group = vim.api.nvim_create_augroup(
+    'comment_string',
+    { clear = true }
+)
+
+-- Nix comment string is a '#'
+vim.api.nvim_create_autocmd(
+    {'BufEnter', 'BufFilePost'},
+    {
+        pattern = '*.nix',
+        callback = function()
+            vim.api.nvim_buf_set_option(0, 'commentstring', '# %s')
+        end,
+        group = commentstring_group
+    }
+)
