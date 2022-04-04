0c40c1e8ffb44a8ac2cf8eeeba625adaabb8cb74
Gaetan Lepage <gaetan.lepage@inria.fr>
2022-04-04 09:42:51 +0200

    [nix] formatting

--- flake.nix
+++ flake.nix
@@ -28,7 +28,7 @@
     outputs = { self, nixpkgs, home-manager, neovim-nightly-overlay }:
         let
 
-        username = "gaetan";
+            username = "gaetan";
             system = "x86_64-linux";
 
             overlays = [
