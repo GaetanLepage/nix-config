756a4f7ab28cfafd6b8f8b80d73baff80a97c123
Gaetan Lepage <gaetan.lepage@inria.fr>
2022-04-04 09:22:03 +0200

    [vim] keymap refactoring

--- config/nvim/lua/utils.lua
+++ config/nvim/lua/utils.lua
@@ -1,32 +1,29 @@
 local M = {}
 
--- function map(mode, key, mode)
---     vim.keymap.set()
--- end
+local function map(mode, key, action)
+    vim.keymap.set(mode, key, action, { silent = true })
+end
+
+-- Public functions to quickly declare keymaps
 
 function M.nmap(key, action)
-    vim.keymap.set('n', key, action,
-                   { silent = true })
+    map('n', key, action)
 end
 
 function M.vmap(key, action)
-    vim.keymap.set('v', key, action,
-                   { silent = true })
+    map('v', key, action)
 end
 
 function M.xmap(key, action)
-    vim.keymap.set('x', key, action,
-                   { silent = true })
+    map('x', key, action)
 end
 
 function M.imap(key, action)
-    vim.keymap.set('i', key, action,
-                   { silent = true })
+    map('i', key, action)
 end
 
 function M.smap(key, action)
-    vim.keymap.set('s', key, action,
-                   { silent = true })
+    map('s', key, action)
 end
 
 return M
