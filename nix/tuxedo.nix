4fa5c7ab39627eca24c3457299da57e54e12c49e
Gaetan Lepage <gaetan.lepage@inria.fr>
2022-04-04 09:43:20 +0200

    [nix] added nix-community binary cache

--- nix/tuxedo.nix
+++ nix/tuxedo.nix
@@ -27,6 +27,14 @@
         '';
 
         trustedUsers = [ "root" "gaetan" ];
+
+        binaryCaches = [
+            "https://nix-community.cachix.org"
+        ];
+
+        binaryCachePublicKeys = [
+            "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        ];
     };
 
 
