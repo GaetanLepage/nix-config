098b2fdb40070d8b99de32d2f1aceadd4cafed78
Gaetan Lepage <gaetan.lepage@inria.fr>
2022-04-16 22:58:40 +0200

    [vim] cleaned markdown preview config

--- config/nvim/lua/plugins/markdown-preview.lua
+++ config/nvim/lua/plugins/markdown-preview.lua
@@ -4,17 +4,3 @@ vim.g.mkdp_browser = 'firefox'
 
 vim.g.mkdp_auto_start = 0
 vim.g.mkdp_auto_stop = 0
-
--- vim.g.mkdp_echo_preview_url = 0
-
--- vim.g.mkdp_open_to_the_world = 1
--- vim.g.mkdp_open_ip = 'localhost'
--- vim.g.mkdp_port = 8081
-
--- vim.cmd [[
--- function! g:EchoUrl(url)
---     :echo a:url
--- endfunction
--- ]]
-
--- vim.g.mkdp_browserfunc = 'g:EchoUrl'
