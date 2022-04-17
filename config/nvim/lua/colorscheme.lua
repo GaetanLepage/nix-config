5a1ff109a4975b97eac311e3499ac26a0d5a7eaf
Gaetan Lepage <gaetan.lepage@inria.fr>
2022-04-17 10:55:55 +0200

    [vim] highlight configs

--- config/nvim/lua/colorscheme.lua
+++ config/nvim/lua/colorscheme.lua
@@ -8,10 +8,11 @@ vim.cmd('colorscheme gruvbox')
 -----------------------
 
 vim.api.nvim_set_hl(0, 'Todo', { fg='Blue', bg='Yellow' })
+vim.fn.matchadd('TODO', 'TODO')
 
 -- Highlight extra whitespaces
 vim.api.nvim_set_hl(0, 'ExtraWhitespace', { bg='Red' })
-vim.cmd[[ match ExtraWhitespace /\s\+\%#\@<!$/ ]]
+vim.fn.matchadd('ExtraWhitespace', '\\s\\+$')
 
 -- Transparent background
 -- vim.api.nvim_set_hl(0, 'Normal', { bg='none' })
