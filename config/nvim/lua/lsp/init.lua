a6ae00e21a2fe3200b30350b3ac9fccf65e85c27
Gaetan Lepage <gaetan.lepage@inria.fr>
2022-04-25 16:42:31 +0200

    [vim] cleaning LSP settings

--- config/nvim/lua/lsp/init.lua
+++ config/nvim/lua/lsp/init.lua
@@ -25,55 +25,54 @@ nmap('<leader>j', vim.diagnostic.goto_next)
 -- Rename
 nmap('<F2>', vim.lsp.buf.rename)
 
--- TODO figure out why this don't work
-vim.fn.sign_define("LspDiagnosticsSignError",
-                   {texthl = "LspDiagnosticsSignError",
-                    text = "",
-                    numhl = "LspDiagnosticsSignError"})
-vim.fn.sign_define("LspDiagnosticsSignWarning",
-                   {texthl = "LspDiagnosticsSignWarning",
-                    text = "",
-                    numhl = "LspDiagnosticsSignWarning"})
-vim.fn.sign_define("LspDiagnosticsSignInformation",
-                   {texthl = "LspDiagnosticsSignInformation",
-                    text = "",
-                    numhl = "LspDiagnosticsSignInformation"})
-vim.fn.sign_define("LspDiagnosticsSignHint",
-                   {texthl = "LspDiagnosticsSignHint",
-                    text = "",
-                    numhl = "LspDiagnosticsSignHint"})
-
 -- scroll up hover doc
--- vim.cmd('nnoremap <silent> <C-b> <cmd>lua require(\'lspsaga.action\').smart_scroll_with_saga(-1)<CR>')
 vim.cmd('command! -nargs=0 LspVirtualTextToggle lua require("lsp/virtual_text").toggle()')
 
-local function documentHighlight(client, bufnr)
+local function highlight_symbol_under_cursor(client, bufnr)
     -- Set autocommands conditional on server_capabilities
     if client.resolved_capabilities.document_highlight then
-        vim.api.nvim_exec([[
-      hi LspReferenceRead cterm=bold ctermbg=red guibg=#464646
-      hi LspReferenceText cterm=bold ctermbg=red guibg=#464646
-      hi LspReferenceWrite cterm=bold ctermbg=red guibg=#464646
-      augroup lsp_document_highlight
-        autocmd! * <buffer>
-        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-      augroup END
-    ]], false)
+
+        vim.api.nvim_set_hl(
+            0, 'LspReferenceRead',
+            { cterm='bold', ctermbg='red', guibg='LightYellow' }
+        )
+        vim.api.nvim_set_hl(
+            0, 'LspReferenceText',
+            { cterm='bold', ctermbg='red', guibg='LightYellow' }
+        )
+        vim.api.nvim_set_hl(
+            0, 'LspReferenceWrite',
+            { cterm='bold', ctermbg='red', guibg='LightYellow' }
+        )
+
+        vim.api.nvim_create_augroup('lsp_document_highlight', {})
+        vim.api.nvim_create_autocmd(
+            {
+                'CursorHold',
+                'CursorHoldI'
+            },
+            {
+                group = 'lsp_document_highlight',
+                buffer = 0,
+                callback = vim.lsp.buf.document_highlight
+            }
+        )
+        vim.api.nvim_create_autocmd(
+            'CursorMoved',
+            {
+                group = 'lsp_document_highlight',
+                buffer = 0,
+                callback = vim.lsp.buf.clear_references
+            }
+        )
 
     end
-
 end
 
 local lsp_config = {}
 
 function lsp_config.common_on_attach(client, bufnr)
-    documentHighlight(client, bufnr)
-end
-
-function lsp_config.tsserver_on_attach(client, bufnr)
-    lsp_config.common_on_attach(client, bufnr)
-    client.resolved_capabilities.document_formatting = false
+    highlight_symbol_under_cursor(client, bufnr)
 end
 
 return lsp_config
