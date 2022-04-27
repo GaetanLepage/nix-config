d20e877db73cea06c0c8f733129641fc2827b6d3
Gaetan Lepage <gaetan.lepage@inria.fr>
2022-04-28 00:08:41 +0200

    [vim] nvim-tree: removed deprecated option auto_resize

--- config/nvim/lua/plugins/nvim-tree.lua
+++ config/nvim/lua/plugins/nvim-tree.lua
@@ -90,7 +90,6 @@ require'nvim-tree'.setup {
         width = 30,             -- width of the window, can be either a number (columns) or a string
                                 --      in `%`
         side = 'left',          -- side of the tree, can be one of 'left', 'right', 'top', 'bottom'
-        auto_resize = false,    -- if true the tree will resize itself after opening a file
 
         mappings = {
 
