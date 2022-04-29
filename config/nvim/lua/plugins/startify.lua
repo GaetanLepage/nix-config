2566c67500a5ec76a337f379087a37a065e0f32d
Gaetan Lepage <gaetan.lepage@inria.fr>
2022-04-29 14:48:28 +0200

    [vim] startify

--- config/nvim/lua/plugins/startify.lua
+++ config/nvim/lua/plugins/startify.lua
@@ -7,24 +7,14 @@ vim.g.startify_custom_header = {
 	' ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀',
 }
 
--- vim.g.webdevicons_enable_startify = 1
--- vim.g.startify_enable_special = 0
--- vim.g.startify_session_dir = '~/.config/nvim/session'
--- vim.g.startify_session_autoload = 1
--- vim.g.startify_session_delete_buffers = 1
-vim.g.startify_change_to_vcs_root = 0
+-- When opening a file or bookmark, change to its directory.
 vim.g.startify_change_to_dir = 0
--- vim.g.startify_fortune_use_unicode = 1
--- vim.g.startify_session_persistence = 1
--- vim.g.startify_session_dir = '~/.config/nvim/session'
 
+-- When opening a file or bookmark, seek and change to the root directory of the VCS
+-- (if there is one).
+vim.g.startify_change_to_vcs_root = 0
 
--- vim.api.nvim_exec('let startify_lists = [ { \'type\': \'files\',     \'header\': [\'   Files\'] }, { \'type\': \'sessions\',  \'header\': [\'   Sessions\'] },    { \'type\': \'bookmarks\', \'header\': [\'   Bookmarks\'] },                                                                   ]',true)
---
--- vim.api.nvim_exec('let startify_bookmarks = [ { \'b\': \'~/Blog\' }, { \'i\': \'~/.config/nvim/init.vim\' }, { \'z\': \'~/.zshrc\' }]', true)
---
--- vim.cmd([[
--- function! StartifyEntryFormat()
---     return 'WebDevIconsGetFileTypeSymbol(absolute_path) ." ". entry_path'
--- endfunction
--- ]])
+-- By default, the fortune header uses ASCII characters, because they work for everyone.
+-- If you set this option to 1 and your 'encoding' is "utf-8", Unicode box-drawing characters will
+-- be used instead.
+vim.g.startify_fortune_use_unicode = 1
