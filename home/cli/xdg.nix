c14b11b67f6a1d86572cf7f4590f1fbdc23f8a9d
Gaetan Lepage <gaetan.lepage@inria.fr>
2022-09-14 08:39:12 +0200

    [home/cli/xdg] gthumb to open figures generated by matplotlib

--- home/cli/xdg.nix
+++ home/cli/xdg.nix
@@ -26,24 +26,25 @@
 
             defaultApplications = {
                 # Applications
-                "application/pdf"   = "org.pwmt.zathura.desktop";
+                "application/pdf"           = "org.pwmt.zathura.desktop";
 
                 # Text
-                "text/html"         = "firefox.desktop";
-                "text/xml"          = "firefox.desktop";
+                "text/html"                 = "firefox.desktop";
+                "text/xml"                  = "firefox.desktop";
 
                 # Images
-                "image/gif"         = "mpvgif.desktop";
-                "image/heif"        = "org.gnome.gThumb.desktop";
-                "image/jpeg"        = "org.gnome.gThumb.desktop";
-                "image/png"         = "org.gnome.gThumb.desktop";
-                "image/webp"        = "org.gnome.gThumb.desktop";
+                "image/gif"                 = "mpvgif.desktop";
+                "image/heif"                = "org.gnome.gThumb.desktop";
+                "image/jpeg"                = "org.gnome.gThumb.desktop";
+                "image/png"                 = "org.gnome.gThumb.desktop";
+                "image/webp"                = "org.gnome.gThumb.desktop";
+                "application/octet-stream"  = "org.gnome.gThumb.desktop"; # matplotlib figures
 
                 # Videos
-                "video/mp4"         = "vlc.desktop";    # .mp4
-                "video/quicktime"   = "vlc.desktop";    # .mov
-                "video/x-matroska"  = "vlc.desktop";    # .mkv
-                "video/x-ms-wmv"    = "vlc.desktop";    # .wmv
+                "video/mp4"                 = "vlc.desktop";    # .mp4
+                "video/quicktime"           = "vlc.desktop";    # .mov
+                "video/x-matroska"          = "vlc.desktop";    # .mkv
+                "video/x-ms-wmv"            = "vlc.desktop";    # .wmv
             };
         };
     };
