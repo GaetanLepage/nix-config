aaf1e127bc6755d3701d62238d3d47b737daae4c
Gaetan Lepage <gaetan@glepage.com>
2022-10-04 17:58:15 +0200

    [nixos/tuxedo/neovim] simplified since not using nightly

--- nixos/tuxedo/neovim.nix
+++ nixos/tuxedo/neovim.nix
@@ -1,21 +1,4 @@
-{ pkgs, ... }:
-
 {
-
-    # Use cachix communnity cache to fetch neovim binaries and avoid compiling it locally.
-    nix.settings = {
-
-        trusted-users = [ "root" "gaetan" ];
-
-        substituters = [
-            "https://nix-community.cachix.org"
-        ];
-
-        trusted-public-keys = [
-            "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        ];
-    };
-
     programs.neovim = {
         enable = true;
 
@@ -23,8 +6,5 @@
 
         viAlias = true;
         vimAlias = true;
-
-        withNodeJs = true;
-        withPython3 = true;
     };
 }
