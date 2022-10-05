a3e75c41bea6a2c03eaa3db26820261c5becfff6
Gaetan Lepage <gaetan@glepage.com>
2022-10-05 23:38:34 +0200

    [nixos/cuda/server-tank-nfs] lazy mounting and auto disconnect

--- nixos/cuda/server-tank-nfs.nix
+++ nixos/cuda/server-tank-nfs.nix
@@ -1,6 +1,16 @@
 {
-    # fileSystems."/mnt/server" = {
-    #     device = "10.10.10.1:/tank";
-    #     fsType = "nfs";
-    # };
+    fileSystems."/mnt/server" = {
+
+        device = "10.10.10.1:/tank";
+
+        fsType = "nfs";
+
+        options = [
+            "noauto"
+            "x-systemd.automount"
+
+            # disconnects after 10 minutes (i.e. 600 seconds)
+            "x-systemd.idle-timeout=600"
+        ];
+    };
 }
