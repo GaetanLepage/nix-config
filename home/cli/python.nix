8fccb7497a7dd361c0e557fb2a768fb3245a41ab
Gaetan Lepage <gaetan@glepage.com>
2022-10-16 19:48:44 +0200

    [home/cli/python] pyp alias

--- home/cli/python.nix
+++ home/cli/python.nix
@@ -1,34 +1,38 @@
 { pkgs, ... }:
 
 {
-    home.packages = with pkgs; [
-        # Python
-        (python3.withPackages(ps: with ps; [
-            # Misc
-            pip
-            setuptools
-            dbus-python             # Needed by the polybar spotify script
+    home = {
+        packages = with pkgs; [
+            # Python
+            (python3.withPackages(ps: with ps; [
+                # Misc
+                pip
+                setuptools
+                dbus-python             # Needed by the polybar spotify script
 
-            # Neovim
-            pynvim                  # Python support for neovim
+                # Neovim
+                pynvim                  # Python support for neovim
 
-            ## Linters
-            flake8
-            mypy
-            pylint
+                ## Linters
+                flake8
+                mypy
+                pylint
 
-            ## Python Language Server
-            pyls-flake8             # Flake8 plugin for pylsp
-            python-lsp-server       # Python language server
-            pylsp-mypy              # mypy plugin for pylsp
+                ## Python Language Server
+                pyls-flake8             # Flake8 plugin for pylsp
+                python-lsp-server       # Python language server
+                pylsp-mypy              # mypy plugin for pylsp
 
-            # RL
-            gym
-            ipython
-            matplotlib
-            numpy
-            pytorch
-            tensorboard
-        ]))
-    ];
+                # RL
+                gym
+                ipython
+                matplotlib
+                numpy
+                pytorch
+                tensorboard
+            ]))
+        ];
+
+        shellAliases."pyp" = "PYTHONPATH=. python";
+    };
 }
