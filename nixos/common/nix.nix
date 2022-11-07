58180ccece73606d1d7bc057a91e5152b1d7fd1c
Gaetan Lepage <gaetan@glepage.com>
2022-11-07 09:33:51 +0100

    [nixos/common/nix] nix settings in attrs

--- nixos/common/nix.nix
+++ nixos/common/nix.nix
@@ -2,29 +2,29 @@
     nixpkgs.config.allowUnfree = true;
 
     nix = {
-        extraOptions = ''
-            experimental-features = nix-command flakes
+
+        settings = {
+            experimental-features = "nix-command flakes";
 
             # Prevent garbage collection from altering nix-shells managed by nix-direnv
             # https://github.com/nix-community/nix-direnv#installation
-            keep-outputs = true
-            keep-derivations = true
+            keep-outputs = true;
+            keep-derivations = true;
 
-            warn-dirty = false
-        '';
-
-        settings = {
             # Automatically detect files in the store that have identical contents, and replaces
             # them with hard links to a single copy. This saves disk space.
             auto-optimise-store = true;
+
+            warn-dirty = false;
         };
 
         gc = {
             automatic = true;
             options = "--delete-older-than 2d";
         };
+
+        # set the value NIX_PATH to whatever nixpkgs our flake uses
+        nixPath = [ "nixpkgs=/etc/nix/inputs/nixpkgs" ];
     };
 
-    # set the value NIX_PATH to whatever nixpkgs our flake uses
-    nix.nixPath = [ "nixpkgs=/etc/nix/inputs/nixpkgs" ];
 }
