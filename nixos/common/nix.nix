40789dc7f7f14a637aa502b91dd0c88cfc7ddbe8
Gaetan Lepage <gaetan@glepage.com>
2022-12-19 10:20:32 +0100

    fix

--- nixos/common/nix.nix
+++ nixos/common/nix.nix
@@ -6,6 +6,8 @@
         settings = {
             experimental-features = "nix-command flakes";
 
+	    # toto
+
             # Prevent garbage collection from altering nix-shells managed by nix-direnv
             # https://github.com/nix-community/nix-direnv#installation
             keep-outputs = true;
@@ -24,7 +26,7 @@
         };
 
         # set the value NIX_PATH to whatever nixpkgs our flake uses
-        nixPath = [ "nixpkgs=/etc/nix/inputs/nixpkgs" ];
+        # nixPath = [ "nixpkgs=/etc/nix/inputs/nixpkgs" ];
     };
 
 }
