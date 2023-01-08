4f8a23e91c457f53b9d3124cd0466b80f6168994
Gaetan Lepage <gaetan@glepage.com>
2023-01-08 21:44:33 +0100

    [nixos/tuxedo/wireguard] make wireguard start automatically

--- nixos/tuxedo/wireguard.nix
+++ nixos/tuxedo/wireguard.nix
@@ -1,11 +1,11 @@
-{ lib, ... }:
-
 {
     networking.wg-quick.interfaces.wg0 = {
         address = [ "10.10.10.2/32" ];
 
         listenPort = 51820;
         dns = [ "10.10.10.1" ];
+
+        autostart = true;
 
         privateKeyFile = "/home/gaetan/config/secrets/wireguard/tuxedo-privatekey";
 
@@ -21,15 +21,11 @@
                 # allowedIPs = [ "10.10.10.0/24" ];
 
                 # Set this to the server IP and port.
-                endpoint = "glepage.com:51820";
+                endpoint = "109.13.20.45:51820";
 
                 # Send keepalives every 25 seconds. Important to keep NAT tables alive.
                 persistentKeepalive = 25;
             }
         ];
-    };
-
-    systemd.services = {
-        wg-quick-wg0.wantedBy = lib.mkForce [ ];
     };
 }
