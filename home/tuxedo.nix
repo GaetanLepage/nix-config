f52a2014dc22afdb113ed9e46c4089970432dc18
Gaetan Lepage <gaetan@glepage.com>
2023-01-12 15:59:30 +0100

    [home/tuxedo] backup script automation

--- home/tuxedo.nix
+++ home/tuxedo.nix
@@ -1,3 +1,4 @@
+{ config, pkgs, ... }:
 {
     imports = [
         ./cli
@@ -21,5 +22,30 @@
         # the Home Manager release notes for a list of state version
         # changes in each release.
         stateVersion = "22.05";
+    };
+
+    # Backup script
+    systemd.user = {
+        services.backup = {
+            Unit.Description = "Home backup script";
+
+            Service = {
+                Type = "oneshot";
+                Environment = "SSH_AUTH_SOCK=/run/user/1000/keyring/ssh";
+                ExecStart = "${pkgs.bash}/bin/bash -l -c ${config.home.homeDirectory}/${config.xdg.configFile.scripts.target}/backup";
+            };
+        };
+
+        timers.backup = {
+
+            Unit.Description = "Home backup script";
+
+            Timer = {
+                OnCalendar = "*-*-* 13:00:00";
+                Unit = "backup.service";
+            };
+
+            Install.WantedBy = [ "timers.target" ];
+        };
     };
 }
