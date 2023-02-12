62328edf92244336a19d1f254d56d37454ee42d8
Gaetan Lepage <gaetan@glepage.com>
2023-02-12 11:26:01 +0100

    [nixos/cuda/ssh] start agent automatically at user login

--- nixos/cuda/ssh.nix
+++ nixos/cuda/ssh.nix
@@ -12,4 +12,10 @@
         gaetan.openssh.authorizedKeys.keys = [ laptopRsaKey ];
         root.openssh.authorizedKeys.keys = [ laptopRsaKey ];
     };
+
+    # Start the OpenSSH agent when you log in.
+    # The OpenSSH agent remembers private keys for you so that you don’t have to type in passphrases
+    # every time you make an SSH connection.
+    # Use ssh-add to add a key to the agent.
+    programs.ssh.startAgent = true;
 }
