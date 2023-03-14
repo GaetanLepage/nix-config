10552066054a7669090bd75b1a9bacb3e1840da8
Gaetan Lepage <gaetan@glepage.com>
2023-03-14 08:57:55 +0100

    [home/tui/neovim] set nowrap

--- home/tui/neovim/options.nix
+++ home/tui/neovim/options.nix
@@ -40,6 +40,7 @@
       fileencoding = "utf-8"; # File-content encoding for the current buffer
       termguicolors = true; # Enables 24-bit RGB color in the |TUI|
       spell = false; # Highlight spelling mistakes (local to window)
+      wrap = false; # Prevent text from wrapping
 
       # Tab options
       tabstop = 4; # Number of spaces a <Tab> in the text stands for (local to buffer)
