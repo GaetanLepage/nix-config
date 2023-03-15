68f952e8cf9d105574cbd0b217e41d02ac0233ec
Gaetan Lepage <gaetan@glepage.com>
2023-03-15 01:52:29 +0100

    [home/gui/gtk] declare file explorer bookmarks

--- home/gui/gtk.nix
+++ home/gui/gtk.nix
@@ -23,6 +23,13 @@
       package = pkgs.papirus-icon-theme;
     };
 
+    gtk3.bookmarks = [
+      "file://${config.xdg.userDirs.download}"
+      "file://${config.home.homeDirectory}/temp"
+      "file:///mnt"
+      "file:///mnt/server"
+    ];
+
     gtk2.configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
   };
 }
