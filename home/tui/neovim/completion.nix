d19b7159fba037ad5139399a61f04ec2ee04fb47
Gaetan Lepage <gaetan@glepage.com>
2023-03-15 13:27:37 +0100

    [home/tui/neovim] add lspkind

--- home/tui/neovim/completion.nix
+++ home/tui/neovim/completion.nix
@@ -5,6 +5,23 @@
     plugins = {
       luasnip.enable = true;
 
+      lspkind = {
+        enable = true;
+
+        cmp = {
+          enable = true;
+          menu = {
+            nvim_lsp = "[LSP]";
+            nvim_lua = "[api]";
+            path = "[path]";
+            luasnip = "[snip]";
+            buffer = "[buffer]";
+            neorg = "[neorg]";
+            cmp_tabnine = "[TabNine]";
+          };
+        };
+      };
+
       nvim-cmp = {
         enable = true;
 
