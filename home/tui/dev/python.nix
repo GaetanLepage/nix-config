ff71a4627745c9f9aa52b2b9b1d5064c36548a0e
Gaetan Lepage <gaetan@glepage.com>
2023-04-03 08:11:19 +0200

    home/tui/dev/python: dependencies for general environment

--- home/tui/dev/python.nix
+++ home/tui/dev/python.nix
@@ -9,25 +9,11 @@
           setuptools
           dbus-python # Needed by the polybar spotify script
 
-          # Neovim
-          pynvim # Python support for neovim
-
-          ## Linters
-          flake8
-          mypy
-          pylint
-
-          ## Python Language Server
-          pyls-flake8 # Flake8 plugin for pylsp
-          python-lsp-server # Python language server
-          pylsp-mypy # mypy plugin for pylsp
-
           # RL
-          gym
-          ipython
+          gymnasium
           matplotlib
           numpy
-          pytorch
+          torch-bin
           tensorboard
         ]))
     ];
