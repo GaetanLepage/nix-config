bed65d65c6eff1e2085a055a771b43bdb29cb0d3
Gaetan Lepage <gaetan@glepage.com>
2023-04-24 11:23:03 +0200

    home/inria: update home-manager stateVersion to 22.11

--- home/inria/default.nix
+++ home/inria/default.nix
@@ -20,7 +20,7 @@
     # You can update Home Manager without changing this value. See
     # the Home Manager release notes for a list of state version
     # changes in each release.
-    stateVersion = "22.05";
+    stateVersion = "22.11";
   };
 
   nix.package = pkgs.nix;
