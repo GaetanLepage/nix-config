e79fcc4d8501dd636b8928b85f0ca64ab628ff8d
Gaetan Lepage <gaetan@glepage.com>
2023-05-12 14:53:55 +0200

    home/tui/neovim/neo-tree: fix nerdfont icons

--- home/tui/neovim/plugins/neo-tree.nix
+++ home/tui/neovim/plugins/neo-tree.nix
@@ -13,6 +13,88 @@
         width = 30;
         autoExpandWidth = true;
       };
+
+      # Fix NerdFont icons
+      defaultComponentConfigs = {
+        icon = {
+          folderEmpty = "󰜌";
+          folderEmptyOpen = "󰜌";
+        };
+        gitStatus = {
+          symbols = {
+            renamed = "󰁕";
+            unstaged = "󰄱";
+          };
+        };
+      };
+      documentSymbols = {
+        kinds = {
+          File = {
+            icon = "󰈙";
+            hl = "Tag";
+          };
+          Namespace = {
+            icon = "󰌗";
+            hl = "Include";
+          };
+          Package = {
+            icon = "󰏖";
+            hl = "Label";
+          };
+          Class = {
+            icon = "󰌗";
+            hl = "Include";
+          };
+          Property = {
+            icon = "󰆧";
+            hl = "@property";
+          };
+          Enum = {
+            icon = "󰒻";
+            hl = "@number";
+          };
+          Function = {
+            icon = "󰊕";
+            hl = "Function";
+          };
+          String = {
+            icon = "󰀬";
+            hl = "String";
+          };
+          Number = {
+            icon = "󰎠";
+            hl = "Number";
+          };
+          Array = {
+            icon = "󰅪";
+            hl = "Type";
+          };
+          Object = {
+            icon = "󰅩";
+            hl = "Type";
+          };
+          Key = {
+            icon = "󰌋";
+            hl = "";
+          };
+          Struct = {
+            icon = "󰌗";
+            hl = "Type";
+          };
+          Operator = {
+            icon = "󰆕";
+            hl = "Operator";
+          };
+          TypeParameter = {
+            icon = "󰊄";
+            hl = "Type";
+          };
+          StaticMethod = {
+            icon = "󰠄 ";
+            hl = "Function";
+          };
+        };
+      };
     };
   };
 }
