421433f39fe5051aaaad5f64c66f9aa5f981c14d
Gaetan Lepage <gaetan@glepage.com>
2023-05-23 17:45:43 +0200

    home/tui/nix: add nixvim cachix

--- home/tui/nix.nix
+++ home/tui/nix.nix
@@ -12,6 +12,7 @@
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+      "nixvim.cachix.org-1:8xrm/43sWNaE3sqFYil49+3wO5LqCbS4FHGhMCuPNNA="
     ];
   };
 }
