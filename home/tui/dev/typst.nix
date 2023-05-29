0ed9762314371e7dcf6c886690ae68af909c5114
Gaetan Lepage <gaetan@glepage.com>
2023-05-29 12:51:51 +0200

    home/tui/dev: add typst-vim plugin

--- home/tui/dev/typst.nix
+++ home/tui/dev/typst.nix
@@ -10,13 +10,17 @@
     # TODO remove when neovim 0.10 is out as this should be the defaut behavior
     filetype.extension."typ" = "typst";
 
-    plugins.lsp.servers = {
-      typst-lsp = {
-        enable = true;
-        rootDir = ''
-          require 'lspconfig.util'.root_pattern('.git', 'main.typ')
-        '';
+    plugins = {
+      lsp.servers = {
+        typst-lsp = {
+          enable = true;
+          rootDir = ''
+            require 'lspconfig.util'.root_pattern('.git', 'main.typ')
+          '';
+        };
       };
+
+      typst-vim.enable = true;
     };
   };
 }
