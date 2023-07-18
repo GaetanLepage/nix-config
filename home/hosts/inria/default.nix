cc050eacd8a3187f2f8209b5b4f9e43cf7e10449
Gaetan Lepage <gaetan@glepage.com>
2023-07-18 09:37:12 +0200

    home/hosts/inria: use conventional/Gnome mime user directories

--- home/hosts/inria/default.nix
+++ home/hosts/inria/default.nix
@@ -1,4 +1,10 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: let
+  homeDirectory = "/home/galepage";
+in {
   imports = [
     ../../modules/tui
     ./update.nix
@@ -8,7 +14,7 @@
   # paths it should manage.
   home = {
     username = "galepage";
-    homeDirectory = "/home/galepage";
+    inherit homeDirectory;
 
     # This value determines the Home Manager release that your
     # configuration is compatible with. This helps avoid breakage
@@ -23,6 +29,17 @@
     sessionVariables = {
       LD_PRELOAD = "/lib/x86_64-linux-gnu/libnss_sss.so.2";
     };
+  };
+
+  xdg.userDirs = with lib; {
+    desktop = mkForce "${homeDirectory}/Desktop";
+    documents = mkForce "${homeDirectory}/Documents";
+    download = mkForce "${homeDirectory}/Downloads";
+    music = mkForce "${homeDirectory}/Music";
+    pictures = mkForce "${homeDirectory}/Pictures";
+    publicShare = mkForce "${homeDirectory}/Public";
+    templates = mkForce "${homeDirectory}/Templates";
+    videos = mkForce "${homeDirectory}/Videos";
   };
 
   nix.package = pkgs.nix;
