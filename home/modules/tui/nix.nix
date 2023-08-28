05024ef184c35211f39c864a3ca8504405fc4457
Gaetan Lepage <gaetan@glepage.com>
2023-08-28 09:38:09 +0200

    home/tui/dev/nix: remove now useless nixvim cachix

--- home/modules/tui/nix.nix
+++ home/modules/tui/nix.nix
@@ -7,14 +7,12 @@
       "https://cache.nixos.org"
       "https://cache.nixos.org/"
       "https://cuda-maintainers.cachix.org"
-      "https://nixvim.cachix.org"
       "https://nix-community.cachix.org"
     ];
 
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
-      "nixvim.cachix.org-1:8xrm/43sWNaE3sqFYil49+3wO5LqCbS4FHGhMCuPNNA="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
