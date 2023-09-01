4621fde536fd80076d071b52eb3ff29bc5798bbd
Gaetan Lepage <gaetan@glepage.com>
2023-09-01 23:27:59 +0200

    home/cuda/csgo: autoexec.cfg

--- home/hosts/cuda/csgo/default.nix
+++ home/hosts/cuda/csgo/default.nix
@@ -1,3 +1,17 @@
-{
-  home.file.".steam/steam/userdata/72487771/730/local/cfg/autoexec.cfg".source = ./csgo-autoexec.cfg;
+{lib, ...}: {
+  xdg.dataFile = let
+    paths =
+      map (
+        profileId: "Steam/userdata/${profileId}/730/local/cfg/autoexec.cfg"
+      )
+      [
+        "72487771" # main
+        "258920647" # zarhos69
+      ];
+  in
+    lib.genAttrs
+    paths
+    (
+      _: {source = ./csgo-autoexec.cfg;}
+    );
 }
