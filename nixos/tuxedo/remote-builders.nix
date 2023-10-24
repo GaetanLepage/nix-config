b4baeaad845a56bb2f9faaf93994b3cf877975eb
Gaetan Lepage <gaetan@glepage.com>
2023-10-24 18:21:54 +0200

    nixos/tuxedo/remote-builders: use alya as the remote builder

--- nixos/tuxedo/remote-builders.nix
+++ nixos/tuxedo/remote-builders.nix
@@ -1,12 +1,26 @@
-{
+{config, ...}: {
+  age.secrets.alya-remote-builder-ssh-key.file = ../../secrets/ssh/alya_remote_builder.age;
+
   nix = {
     distributedBuilds = true;
     buildMachines = [
+      # {
+      #   hostName = "10.10.10.5";
+      #   sshUser = "nix";
+      #   system = "x86_64-linux";
+      #   maxJobs = 12;
+      #   supportedFeatures = [
+      #     "kvm"
+      #     "big-parallel"
+      #     "benchmark"
+      #   ];
+      # }
       {
-        hostName = "10.10.10.5";
-        sshUser = "nix";
+        hostName = "10.10.10.4";
+        sshUser = "root";
+        sshKey = config.age.secrets.alya-remote-builder-ssh-key.path;
         system = "x86_64-linux";
-        maxJobs = 12;
+        maxJobs = 16;
         supportedFeatures = [
           "kvm"
           "big-parallel"
