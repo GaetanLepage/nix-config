461bf90fa1763ebc884bc5fabf4fa0dd594ad399
Gaetan Lepage <gaetan@glepage.com>
2023-11-16 09:24:23 +0100

    nixos/tuxedo/remote-builders: use ssh-ng protocol

--- nixos/tuxedo/remote-builders.nix
+++ nixos/tuxedo/remote-builders.nix
@@ -9,80 +9,85 @@
 
   nix = {
     distributedBuilds = true;
-    buildMachines = [
-      {
-        # cuda
-        hostName = "10.10.10.5";
-        sshUser = "nix";
-        sshKey = config.age.secrets.cuda-remote-builder-ssh-key.path;
-        publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSURKMjVOWVVUSHRuTlpZOVdDYW12OFpHYzAwekc0RXFKaHllam8zVG1xT3UK";
-        system = "x86_64-linux";
-        maxJobs = 12;
-        supportedFeatures = [
-          "kvm"
-          "big-parallel"
-          "benchmark"
-        ];
-      }
-      {
-        # Alya
-        hostName = "10.10.10.4";
-        sshUser = "root";
-        sshKey = config.age.secrets.alya-remote-builder-ssh-key.path;
-        publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUxLTnozOWJFSzR4M1JWeHEvekFyb1A2ZE13VFdUd2FabTBJUi9lTmFJeHYK";
-        system = "x86_64-linux";
-        maxJobs = 16;
-        supportedFeatures = [
-          "kvm"
-          "big-parallel"
-          "benchmark"
-        ];
-      }
-      {
-        # ARM build box
-        hostName = "aarch64.nixos.community";
-        sshUser = "glepage";
-        sshKey = config.age.secrets.aarch64-build-box-ssh-key.path;
-        publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSU1VVHo1aTl1NUgyRkhOQW1aSnlvSmZJR3lVbS9IZkdoZnduYzE0MkwzZHMK";
-        systems = [
-          "aarch64-linux"
-        ];
-        maxJobs = 64;
-        supportedFeatures = [
-          "big-parallel"
-        ];
-      }
-      {
-        # Darwin build box
-        hostName = "darwin-build-box.nix-community.org";
-        sshUser = "glepage";
-        sshKey = config.age.secrets.darwin-build-box-ssh-key.path;
-        publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUtYN1cxenR6QXRWWFQrTkJNSVRVK0pMWGNJRTVIVEVPZDdRM2ZRTnU4MFMK";
-        maxJobs = 32;
-        systems = [
-          "aarch64-darwin"
-          "x86_64-darwin"
-        ];
-      }
-      {
-        # Linux build box
-        hostName = "build-box.nix-community.org";
-        sshUser = "glepage";
-        sshKey = config.age.secrets.linux-build-box-ssh-key.path;
-        publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUVsSVE1NHFBeTdEaDYzckJ1ZFlLZGJ6SkhycmJyck1YTFlsN1BrbWs4OEgK";
-        maxJobs = 16;
-        systems = [
-          "i686-linux"
-          "riscv64-linux"
-          "x86_64-linux"
-        ];
-        supportedFeatures = [
-          "benchmark"
-          "big-parallel"
-          "kvm"
-          "nixos-test"
-        ];
-      }
-    ];
+    buildMachines =
+      map
+      (builder:
+        {
+          sshUser = "glepage";
+          protocol = "ssh-ng";
+        }
+        // builder)
+      [
+        {
+          # cuda
+          hostName = "10.10.10.5";
+          sshUser = "nix";
+          sshKey = config.age.secrets.cuda-remote-builder-ssh-key.path;
+          publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSURKMjVOWVVUSHRuTlpZOVdDYW12OFpHYzAwekc0RXFKaHllam8zVG1xT3UK";
+          system = "x86_64-linux";
+          maxJobs = 12;
+          supportedFeatures = [
+            "kvm"
+            "big-parallel"
+            "benchmark"
+          ];
+        }
+        {
+          # Alya
+          hostName = "10.10.10.4";
+          sshUser = "root";
+          sshKey = config.age.secrets.alya-remote-builder-ssh-key.path;
+          publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUxLTnozOWJFSzR4M1JWeHEvekFyb1A2ZE13VFdUd2FabTBJUi9lTmFJeHYK";
+          system = "x86_64-linux";
+          maxJobs = 16;
+          supportedFeatures = [
+            "kvm"
+            "big-parallel"
+            "benchmark"
+          ];
+        }
+        {
+          # ARM build box
+          hostName = "aarch64.nixos.community";
+          sshKey = config.age.secrets.aarch64-build-box-ssh-key.path;
+          publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSU1VVHo1aTl1NUgyRkhOQW1aSnlvSmZJR3lVbS9IZkdoZnduYzE0MkwzZHMK";
+          systems = [
+            "aarch64-linux"
+          ];
+          maxJobs = 64;
+          supportedFeatures = [
+            "big-parallel"
+          ];
+        }
+        {
+          # Darwin build box
+          hostName = "darwin-build-box.nix-community.org";
+          sshKey = config.age.secrets.darwin-build-box-ssh-key.path;
+          publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUtYN1cxenR6QXRWWFQrTkJNSVRVK0pMWGNJRTVIVEVPZDdRM2ZRTnU4MFMK";
+          maxJobs = 32;
+          systems = [
+            "aarch64-darwin"
+            "x86_64-darwin"
+          ];
+        }
+        {
+          # Linux build box
+          hostName = "build-box.nix-community.org";
+          sshKey = config.age.secrets.linux-build-box-ssh-key.path;
+          publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUVsSVE1NHFBeTdEaDYzckJ1ZFlLZGJ6SkhycmJyck1YTFlsN1BrbWs4OEgK";
+          maxJobs = 16;
+          systems = [
+            "i686-linux"
+            "riscv64-linux"
+            "x86_64-linux"
+          ];
+          supportedFeatures = [
+            "benchmark"
+            "big-parallel"
+            "kvm"
+            "nixos-test"
+          ];
+        }
+      ];
   };
 }
