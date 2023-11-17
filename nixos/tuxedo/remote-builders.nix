a1bcf78f7b084e84cc8b3c5ef747336ec301bbf8
Gaetan Lepage <gaetan@glepage.com>
2023-11-17 11:26:41 +0100

    nixos/tuxedo/remote-builders: allow builders to use their own subsitutes

--- nixos/tuxedo/remote-builders.nix
+++ nixos/tuxedo/remote-builders.nix
@@ -10,6 +10,13 @@
   nix = {
     distributedBuilds = true;
 
+    # Nix will instruct remote build machines to use their own binary substitutes if available.
+    # In practical terms, this means that remote hosts will fetch as many build dependencies as
+    # possible from their own substitutes (e.g, from cache.nixos.org), instead of waiting for this
+    # host to upload them all. This can drastically reduce build times if the network connection
+    # between this computer and the remote build host is slow.
+    settings.builders-use-substitutes = true;
+
     buildMachines = [
       {
         # cuda
