6a13c9e00632bcdc346a6542bd43828c3d88ca67
Gaetan Lepage <gaetan@glepage.com>
2023-11-23 00:25:29 +0100

    home/tui/dev/python: switch to the HM ruff module

--- home/modules/tui/dev/python.nix
+++ home/modules/tui/dev/python.nix
@@ -1,8 +1,6 @@
 {pkgs, ...}: {
   home = {
     packages = with pkgs; [
-      ruff
-
       # Python
       (python3.withPackages (ps:
         with ps; [
@@ -24,54 +22,54 @@
     };
   };
 
-  xdg.configFile."ruff/ruff.toml".text = ''
-    line-length = 100
+  programs = {
+    ruff = {
+      enable = true;
 
-    [per-file-ignores]
-    # Ignore `F401` (unused import) in all `__init__.py` files
-    "__init__.py" = ["F401"]
-  '';
-
-  programs.nixvim = {
-    filetype.extension.gin = "gin";
-    plugins = {
-      efmls-configs.setup.python = {
-        formatter = "ruff";
-        linter = [
-          "mypy"
-        ];
+      settings = {
+        line-length = 100;
+        per-file-ignores = {
+          # Ignore `F401` (unused import) in all `__init__.py` files
+          "__init__.py" = ["F401"];
+        };
       };
-      treesitter.languageRegister.python = ["gin"];
-      efmls-configs.setup.python.formatter = "ruff";
-      lsp.servers = {
-        efm.filetypes = ["python"];
-        pylsp = {
-          # cmd = ["pylsp" "-vvv"];
-          # cmd = ["pylsp" "-vvv" "--log-file" "pylsp.log"];
-          enable = true;
-          settings = {
-            plugins = {
-              jedi_completion.fuzzy = true;
+    };
 
-              pylsp_mypy.enabled = true;
-              ruff = {
-                enabled = false;
-                lineLength = 100;
-                perFileIgnores = {
-                  "__init__.py" = ["F401"];
+    nixvim = {
+      filetype.extension.gin = "gin";
+      plugins = {
+        treesitter.languageRegister.python = ["gin"];
+        efmls-configs.setup.python.formatter = "ruff";
+        lsp.servers = {
+          efm.filetypes = ["python"];
+          pylsp = {
+            # cmd = ["pylsp" "-vvv"];
+            # cmd = ["pylsp" "-vvv" "--log-file" "pylsp.log"];
+            enable = true;
+            settings = {
+              plugins = {
+                jedi_completion.fuzzy = true;
+
+                pylsp_mypy.enabled = true;
+                ruff = {
+                  enabled = false;
+                  lineLength = 100;
+                  perFileIgnores = {
+                    "__init__.py" = ["F401"];
+                  };
                 };
-              };
 
-              # We don't need those as ruff is already providing such features.
-              autopep8.enabled = false;
-              flake8.enabled = false;
-              mccabe.enabled = false;
-              preload.enabled = false;
-              pycodestyle.enabled = false;
-              pydocstyle.enabled = false;
-              pyflakes.enabled = false;
-              pylint.enabled = false;
-              yapf.enabled = false;
+                # We don't need those as ruff is already providing such features.
+                autopep8.enabled = false;
+                flake8.enabled = false;
+                mccabe.enabled = false;
+                preload.enabled = false;
+                pycodestyle.enabled = false;
+                pydocstyle.enabled = false;
+                pyflakes.enabled = false;
+                pylint.enabled = false;
+                yapf.enabled = false;
+              };
             };
           };
         };
