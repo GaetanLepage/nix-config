f8fa36476370baf9a31d31276267f18c378f8ccd
Gaetan Lepage <gaetan@glepage.com>
2023-11-23 14:57:51 +0100

    nixos/cuda/github-runner: remove node16 as it is not supported anymore

--- nixos/cuda/github-runner.nix
+++ nixos/cuda/github-runner.nix
@@ -7,10 +7,6 @@
 in {
   age.secrets.github-runner-nixvim-token.file = ../../secrets/github-runner-nixvim-token.age;
 
-  nixpkgs.config.permittedInsecurePackages = [
-    "nodejs-16.20.2"
-  ];
-
   nix.settings.trusted-users = [
     "github-runner-${runnerName}"
   ];
@@ -22,10 +18,6 @@ in {
     url = "https://github.com/nix-community/nixvim";
     tokenFile = config.age.secrets.github-runner-nixvim-token.path;
 
-    nodeRuntimes = [
-      "node16"
-      "node20"
-    ];
     extraPackages = with pkgs; [
       cachix
     ];
