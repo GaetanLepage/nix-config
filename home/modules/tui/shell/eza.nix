87395b785e775abbb842db989d09e678d4dbd041
Gaetan Lepage <gaetan@glepage.com>
2024-01-26 16:37:01 +0100

    home/tui/shell/eza: simplify aliases definitions

--- home/modules/tui/shell/eza.nix
+++ home/modules/tui/shell/eza.nix
@@ -1,9 +1,9 @@
-{pkgs, ...}: {
+{
   programs.eza.enable = true;
 
   home.shellAliases = {
-    ls = "${pkgs.eza}/bin/eza";
-    l = "${pkgs.eza}/bin/eza -lFhg";
-    ll = "${pkgs.eza}/bin/eza -alFhg";
+    ls = "eza";
+    l = "eza -lFhg";
+    ll = "eza -alFhg";
   };
 }
