6f63774a31e4053fed39500bce21bb299106efb2
Gaetan Lepage <gaetan@glepage.com>
2024-02-07 13:39:53 +0100

    home/tui/neovim: remove useless 'softtabstop' setting to its default value

--- home/modules/tui/neovim/options.nix
+++ home/modules/tui/neovim/options.nix
@@ -50,7 +50,6 @@
       # Tab options
       tabstop = 4; # Number of spaces a <Tab> in the text stands for (local to buffer)
       shiftwidth = 4; # Number of spaces used for each step of (auto)indent (local to buffer)
-      softtabstop = 0; # If non-zero, number of spaces to insert for a <Tab> (local to buffer)
       expandtab = true; # Expand <Tab> to spaces in Insert mode (local to buffer)
       autoindent = true; # Do clever autoindenting
 
