7855d6e3f082cbdb1a20142a8299cb33729366ab
Gaetan Lepage <gaetan@glepage.com>
2024-02-09 22:59:04 +0100

    nixos/common/fish: remove useless comment

--- nixos/common/fish.nix
+++ nixos/common/fish.nix
@@ -1,7 +1,6 @@
 {pkgs, ...}: {
   users.users.gaetan.shell = pkgs.fish;
 
-  # https://github.com/NixOS/nixpkgs/issues/10349#issuecomment-341810990
   programs.fish.enable = true;
 
   environment.pathsToLink = ["/share/fish"];
