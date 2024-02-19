461287ff8ddf051c33ea973a36c11fd9a3d09478
Gaetan Lepage <gaetan@glepage.com>
2024-02-19 07:56:47 +0100

    home/tui/neovim/treesitter: set highlightDefinitions.clearOnCursorMove to true

--- home/modules/tui/neovim/plugins/treesitter.nix
+++ home/modules/tui/neovim/plugins/treesitter.nix
@@ -11,7 +11,11 @@
 
     treesitter-refactor = {
       enable = true;
-      highlightDefinitions.enable = true;
+      highlightDefinitions = {
+        enable = true;
+        # Set to false if you have an `updatetime` of ~100.
+        clearOnCursorMove = false;
+      };
     };
 
     hmts.enable = true;
