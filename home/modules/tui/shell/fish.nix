41345f13cac129db9ec221e5da799478cb753ada
Gaetan Lepage <gaetan@glepage.com>
2024-02-20 20:54:43 +0100

    home/tui/shell/fish: use interactiveShellInit instead of if statement

--- home/modules/tui/shell/fish.nix
+++ home/modules/tui/shell/fish.nix
@@ -3,34 +3,32 @@
     fish = {
       enable = true;
 
-      shellInit = ''
-        if status is-interactive
-          ${pkgs.pfetch}/bin/pfetch
+      interactiveShellInit = ''
+        ${pkgs.pfetch}/bin/pfetch
 
-          # don't greet
-          function fish_greeting; end
+        # don't greet
+        function fish_greeting; end
 
-          # Map '-' to cd back to previous directory
-          abbr -a -- - prevd
+        # Map '-' to cd back to previous directory
+        abbr -a -- - prevd
 
-          # use vi bindings
-          set -g fish_key_bindings fish_vi_key_bindings
+        # use vi bindings
+        set -g fish_key_bindings fish_vi_key_bindings
 
-          # Emulates vim's cursor shape behavior
-          set -g fish_vi_force_cursor 1
-          # Set the normal and visual mode cursors to a block
-          set fish_cursor_default block
-          # Set the insert mode cursor to a line
-          set fish_cursor_insert line
-          # Set the replace mode cursors to an underscore
-          set fish_cursor_replace_one underscore
-          set fish_cursor_replace underscore
+        # Emulates vim's cursor shape behavior
+        set -g fish_vi_force_cursor 1
+        # Set the normal and visual mode cursors to a block
+        set fish_cursor_default block
+        # Set the insert mode cursor to a line
+        set fish_cursor_insert line
+        # Set the replace mode cursors to an underscore
+        set fish_cursor_replace_one underscore
+        set fish_cursor_replace underscore
 
-          function multicd
-            echo cd (string repeat -n (math (string length -- $argv[1]) - 1) ../)
-          end
-          abbr --add dotdot --regex '^\.\.+$' --function multicd
+        function multicd
+          echo cd (string repeat -n (math (string length -- $argv[1]) - 1) ../)
         end
+        abbr --add dotdot --regex '^\.\.+$' --function multicd
       '';
 
       plugins = [
