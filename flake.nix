51c371731c41316f8ccf07ddda79269ab60beeaf
Gaetan Lepage <gaetan@glepage.com>
2024-02-21 15:53:08 +0100

    flake.nix: switch to flake-parts

--- flake.nix
+++ flake.nix
@@ -17,6 +17,11 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     # nixpkgs.url = "/home/gaetan/perso/nix/nixpkgs";
 
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
     home-manager = {
       url = "github:nix-community/home-manager";
       # url = "/home/gaetan/perso/nix/home-manager";
@@ -43,60 +48,73 @@
   outputs = {
     self,
     nixpkgs,
+    flake-parts,
     home-manager,
     agenix,
     nixvim,
     nix-index-database,
-  }: let
-    system = "x86_64-linux";
-    inherit (import nixpkgs {inherit system;}) lib;
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = nixpkgs.lib.systems.flakeExposed;
 
-    homeManagerModules = hostname: [
-      nixvim.homeManagerModules.nixvim
-      nix-index-database.hmModules.nix-index
-      ./home/hosts/${hostname}
-    ];
-  in {
-    formatter.${system} = nixpkgs.legacyPackages.${system}.alejandra;
-
-    nixosConfigurations = let
-      mkHost = hostname:
-        nixpkgs.lib.nixosSystem {
-          inherit system;
-
-          modules = [
-            # The system configuration
-            ./nixos/${hostname}
-            {nix.nixPath = ["nixpkgs=${nixpkgs.outPath}"];}
-
-            # agenix
-            agenix.nixosModules.default
-            {environment.systemPackages = [agenix.packages.x86_64-linux.default];}
-
-            # Home manager configuration
-            home-manager.nixosModules.home-manager
-            {
-              home-manager = {
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                users.gaetan.imports = homeManagerModules hostname;
-              };
-            }
-          ];
-        };
-    in
-      lib.genAttrs ["tuxedo" "cuda"] mkHost;
-
-    # Inria
-    homeConfigurations.inria = home-manager.lib.homeManagerConfiguration {
-      pkgs = import nixpkgs {
+      flake = let
         system = "x86_64-linux";
-        config = {
-          allowUnfree = true;
-          cudaSupport = true;
+        homeManagerModules = hostname: [
+          nixvim.homeManagerModules.nixvim
+          nix-index-database.hmModules.nix-index
+          ./home/hosts/${hostname}
+        ];
+      in {
+        nixosConfigurations = let
+          mkHost = hostname:
+            nixpkgs.lib.nixosSystem {
+              inherit system;
+
+              modules = [
+                # The system configuration
+                ./nixos/${hostname}
+                {nix.nixPath = ["nixpkgs=${nixpkgs.outPath}"];}
+
+                # agenix
+                agenix.nixosModules.default
+                {environment.systemPackages = [agenix.packages.x86_64-linux.default];}
+
+                # Home manager configuration
+                home-manager.nixosModules.home-manager
+                {
+                  home-manager = {
+                    useGlobalPkgs = true;
+                    useUserPackages = true;
+                    users.gaetan.imports = homeManagerModules hostname;
+                  };
+                }
+              ];
+            };
+        in {
+          tuxedo = mkHost "tuxedo";
+          cuda = mkHost "cuda";
+        };
+
+        # Inria
+        homeConfigurations.inria = home-manager.lib.homeManagerConfiguration {
+          pkgs = import nixpkgs {
+            system = "x86_64-linux";
+            config = {
+              allowUnfree = true;
+              cudaSupport = true;
+            };
+          };
+          modules = homeManagerModules "inria";
         };
       };
-      modules = homeManagerModules "inria";
+
+      perSystem = {
+        config,
+        pkgs,
+        system,
+        ...
+      }: {
+        formatter = pkgs.alejandra;
+      };
     };
-  };
 }
