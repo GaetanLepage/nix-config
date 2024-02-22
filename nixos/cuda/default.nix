325bd7b11b2a2f44a99bdef98de2c226fcb1d778
Gaetan Lepage <gaetan@glepage.com>
2024-02-22 18:05:14 +0100

    nixos/cuda: unset hostPubkey for installation

--- nixos/cuda/default.nix
+++ nixos/cuda/default.nix
@@ -20,7 +20,7 @@
   ];
 
   networking.hostName = "cuda";
-  age.rekey.hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDJ25NYUTHtnNZY9WCamv8ZGc00zG4EqJhyejo3TmqOu";
+  # age.rekey.hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDJ25NYUTHtnNZY9WCamv8ZGc00zG4EqJhyejo3TmqOu";
 
   environment.systemPackages = with pkgs; [
     # Utils
