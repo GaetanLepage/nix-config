09126b762aa5bf1c0c62557aa744fca2999f3b53
Gaetan Lepage <gaetan@glepage.com>
2024-02-25 10:14:30 +0000

    framework: stateVersion 24.05

--- nixos/framework/default.nix
+++ nixos/framework/default.nix
@@ -51,6 +51,6 @@
     # this value at the release version of the first install of this system.
     # Before changing this value read the documentation for this option
     # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-    stateVersion = "23.05"; # Did you read the comment?
+    stateVersion = "24.05"; # Did you read the comment?
   };
 }
