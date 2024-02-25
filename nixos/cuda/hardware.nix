c36e700087d15c29355f0311e62f54b8c1564f11
Gaetan Lepage <gaetan@glepage.com>
2024-02-25 12:28:14 +0100

    cuda/hardware: update

--- nixos/cuda/hardware.nix
+++ nixos/cuda/hardware.nix
@@ -24,7 +24,8 @@
     extraModulePackages = [];
   };
 
-  hardware = {
-    cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-  };
+  networking.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
