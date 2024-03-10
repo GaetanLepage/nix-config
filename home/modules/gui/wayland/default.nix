184c0075036cec0df41271332c9a23528f9183a8
Gaetan Lepage <gaetan@glepage.com>
2024-03-10 19:08:34 +0100

    home/gui/wayland: formatting

--- home/modules/gui/wayland/default.nix
+++ home/modules/gui/wayland/default.nix
@@ -10,20 +10,22 @@
 
   programs.rofi.package = pkgs.rofi-wayland;
 
-  home.packages = with pkgs; [
-    wdisplays
-    wlr-randr
-    wl-clipboard
-    grim # needed by flameshot
-  ];
+  home = {
+    packages = with pkgs; [
+      wdisplays
+      wlr-randr
+      wl-clipboard
+      grim # needed by flameshot
+    ];
 
-  home.sessionVariables = {
-    NIXOS_OZONE_WL = 1;
+    sessionVariables = {
+      NIXOS_OZONE_WL = 1;
 
-    SDL_VIDEODRIVER = "wayland";
-    QT_QPA_PLATFORM = "wayland";
-    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
-    _JAVA_AWT_WM_NONREPARENTING = "1";
-    XDG_SESSION_TYPE = "wayland";
+      SDL_VIDEODRIVER = "wayland";
+      QT_QPA_PLATFORM = "wayland";
+      QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+      _JAVA_AWT_WM_NONREPARENTING = "1";
+      XDG_SESSION_TYPE = "wayland";
+    };
   };
 }
