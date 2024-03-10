9a85ed62fbc65693ed8d78bfd2b76d3c0ab6808a
Gaetan Lepage <gaetan@glepage.com>
2024-03-10 18:53:53 +0100

    nixos/framework/obs: remove intel driver stuff

--- nixos/framework/obs.nix
+++ nixos/framework/obs.nix
@@ -6,14 +6,4 @@
       ];
     })
   ];
-
-  # hardware.opengl = {
-  #   enable = true;
-  #   # extraPackages = with pkgs; [
-  #   #   intel-media-driver # LIBVA_DRIVER_NAME=iHD
-  #   #   vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
-  #   #   vaapiVdpau
-  #   #   libvdpau-va-gl
-  #   # ];
-  # };
 }
