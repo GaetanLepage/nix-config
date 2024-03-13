27b694ad1dc764a70613769b35099dff6e373248
Gaetan Lepage <gaetan@glepage.com>
2024-03-13 10:03:35 +0100

    home/tui/neovim/startify: adapt to nixvim changes

--- home/modules/tui/neovim/plugins/startify.nix
+++ home/modules/tui/neovim/plugins/startify.nix
@@ -2,22 +2,24 @@
   programs.nixvim.plugins.startify = {
     enable = true;
 
-    customHeader = [
-      ""
-      "     ███╗   ██╗██╗██╗  ██╗██╗   ██╗██╗███╗   ███╗"
-      "     ████╗  ██║██║╚██╗██╔╝██║   ██║██║████╗ ████║"
-      "     ██╔██╗ ██║██║ ╚███╔╝ ██║   ██║██║██╔████╔██║"
-      "     ██║╚██╗██║██║ ██╔██╗ ╚██╗ ██╔╝██║██║╚██╔╝██║"
-      "     ██║ ╚████║██║██╔╝ ██╗ ╚████╔╝ ██║██║ ╚═╝ ██║"
-      "     ╚═╝  ╚═══╝╚═╝╚═╝  ╚═╝  ╚═══╝  ╚═╝╚═╝     ╚═╝"
-    ];
+    settings = {
+      custom_header = [
+        ""
+        "     ███╗   ██╗██╗██╗  ██╗██╗   ██╗██╗███╗   ███╗"
+        "     ████╗  ██║██║╚██╗██╔╝██║   ██║██║████╗ ████║"
+        "     ██╔██╗ ██║██║ ╚███╔╝ ██║   ██║██║██╔████╔██║"
+        "     ██║╚██╗██║██║ ██╔██╗ ╚██╗ ██╔╝██║██║╚██╔╝██║"
+        "     ██║ ╚████║██║██╔╝ ██╗ ╚████╔╝ ██║██║ ╚═╝ ██║"
+        "     ╚═╝  ╚═══╝╚═╝╚═╝  ╚═╝  ╚═══╝  ╚═╝╚═╝     ╚═╝"
+      ];
 
-    # When opening a file or bookmark, change to its directory.
-    changeToDir = false;
+      # When opening a file or bookmark, change to its directory.
+      change_to_dir = false;
 
-    # By default, the fortune header uses ASCII characters, because they work for everyone.
-    # If you set this option to 1 and your 'encoding' is "utf-8", Unicode box-drawing characters will
-    # be used instead.
-    useUnicode = true;
+      # By default, the fortune header uses ASCII characters, because they work for everyone.
+      # If you set this option to 1 and your 'encoding' is "utf-8", Unicode box-drawing characters will
+      # be used instead.
+      use_unicode = true;
+    };
   };
 }
