e94024925d1c31ac8baa1def27c8cd75179c25c9
Gaetan Lepage <gaetan@glepage.com>
2024-03-13 10:02:05 +0100

    home/tui/shell/eza: disable clashing fish integration

--- home/modules/tui/shell/eza.nix
+++ home/modules/tui/shell/eza.nix
@@ -1,9 +1,13 @@
 {
-  programs.eza.enable = true;
+  programs.eza = {
+    enable = true;
+    enableFishIntegration = false;
+  };
 
   home.shellAliases = {
     ls = "eza";
     l = "eza -lhg";
     ll = "eza -alhg";
+    lt = "eza --tree";
   };
 }
