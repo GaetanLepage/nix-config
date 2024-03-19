c8a1e94d2426b686080de7d59bb4c0582dd5c2e5
Gaetan Lepage <gaetan@glepage.com>
2024-03-19 13:16:24 +0100

    home/gui/wayland/sway/swaylock: fix wallpaper path

--- home/modules/gui/wayland/sway/swaylock.nix
+++ home/modules/gui/wayland/sway/swaylock.nix
@@ -5,7 +5,7 @@
     settings = {
       daemonize = true;
 
-      image = builtins.toString ./wallpaper.png;
+      image = builtins.toString ../wallpaper.png;
       indicator-x-position = 100;
       indicator-y-position = 100;
     };
