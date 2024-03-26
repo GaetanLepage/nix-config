a89902a9f885a4d7d0cfbec967f794930729a37d
Gaetan Lepage <gaetan@glepage.com>
2024-03-26 17:38:40 +0100

    home/gui/wayland/waybar: add the right number of cores for CPU module

--- home/modules/gui/wayland/waybar/modules/default.nix
+++ home/modules/gui/wayland/waybar/modules/default.nix
@@ -35,7 +35,7 @@ pkgs: {
     interval = 1;
     tooltip = false;
     # format = " {usage:02}%";
-    format = "󰍛 {icon0}{icon1}{icon2}{icon3}{icon4}{icon5}{icon6}{icon7} {usage:02}%";
+    format = "󰍛 {icon0}{icon1}{icon2}{icon3}{icon4}{icon5}{icon6}{icon7}{icon8}{icon9}{icon10}{icon11}{icon12}{icon13}{icon14}{icon15} {usage:02}%";
     format-icons = ["▁" "▂" "▃" "▄" "▅" "▆" "▇" "█"];
   };
 
