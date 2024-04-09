d791a8454851ac3ed82e50a6842c414c4cb96fb4
Gaetan Lepage <gaetan@glepage.com>
2024-04-09 16:31:55 +0200

    nixos/chamaeleon/remote-builders: add gpu8

--- nixos/chamaeleon/remote-builders/default.nix
+++ nixos/chamaeleon/remote-builders/default.nix
@@ -3,18 +3,29 @@
     builder-ssh-key.rekeyFile = ./ssh-key.age;
   };
 
-  nix.buildMachines = [
-    {
-      hostName = "auriga";
-      sshUser = "root";
-      sshKey = config.age.secrets.builder-ssh-key.path;
-      publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUU3Vi9GRW9EQmxwQWg3NzM3VjJRcTYzbXA2Nk85WVhMRWRBREIzRVZEMmggcm9vdEBwb3Atb3MK";
-      systems = ["x86_64-linux"];
-      maxJobs = 48;
-      speedFactor = 8;
-      supportedFeatures = [
-        "big-parallel"
-      ];
-    }
-  ];
+  nix.buildMachines =
+    builtins.map (conf:
+      {
+        sshUser = "root";
+        sshKey = config.age.secrets.builder-ssh-key.path;
+        systems = ["x86_64-linux"];
+        supportedFeatures = [
+          "big-parallel"
+        ];
+      }
+      // conf)
+    [
+      {
+        hostName = "auriga";
+        publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUU3Vi9GRW9EQmxwQWg3NzM3VjJRcTYzbXA2Nk85WVhMRWRBREIzRVZEMmggcm9vdEBwb3Atb3MK";
+        maxJobs = 48;
+        speedFactor = 3;
+      }
+      {
+        hostName = "gpu8-perception";
+        publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSVBmTnFIQVNqcXRFNnNJTlM4b2c0bS9qaW0rUjVzTStHcVc5blRkaTNEM24gcm9vdEB1YnVudHUtc2VydmVyCg==";
+        maxJobs = 40;
+        speedFactor = 2;
+      }
+    ];
 }
