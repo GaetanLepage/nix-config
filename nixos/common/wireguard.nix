9aff0fe96f61ada8ae5b0cab71f889f8ede6df4d
Gaetan Lepage <gaetan@glepage.com>
2024-04-27 18:52:57 +0200

    nixos/common/wireguard: refactor

--- nixos/common/wireguard.nix
+++ nixos/common/wireguard.nix
@@ -15,42 +15,25 @@
     cfg = config.my-modules.wireguard;
   in
     lib.mkIf cfg.enable {
-      networking = {
-        firewall.allowedUDPPorts = [51820];
+      networking.wireguard.interfaces.wg0 = {
+        ips = ["${cfg.ip}/32"];
+        listenPort = 51820;
+        privateKeyFile = config.age.secrets.wireguard-private-key.path;
 
-        # "wg0" is the network interface name. You can name the interface arbitrarily.
-        wireguard.interfaces.wg0 = {
-          # Determines the IP address and subnet of the client's end of the tunnel interface.
-          ips = ["${cfg.ip}/32"];
-          listenPort = 51820;
+        peers = [
+          {
+            endpoint = "glepage.com:51820";
+            publicKey = "jWzlVwkNkaO1uj7Qh+Xemo0EtxIYP2ufK+18oPcdvBY=";
 
-          # Path to the private key file.
-          #
-          # Note: The private key can also be included inline via the privateKey option,
-          # but this makes the private key world-readable; thus, using privateKeyFile is
-          # recommended.
-          privateKeyFile = config.age.secrets.wireguard-private-key.path;
+            # Forward all the traffic via VPN.
+            #allowedIPs = [ "0.0.0.0/0" ];
+            # Or forward only particular subnets
+            allowedIPs = ["10.10.10.0/24"];
 
-          peers = [
-            # For a client configuration, one peer entry for the server will suffice.
-
-            {
-              # Public key of the server (not a file path).
-              publicKey = "jWzlVwkNkaO1uj7Qh+Xemo0EtxIYP2ufK+18oPcdvBY=";
-
-              # Forward all the traffic via VPN.
-              #allowedIPs = [ "0.0.0.0/0" ];
-              # Or forward only particular subnets
-              allowedIPs = ["10.10.10.0/24"];
-
-              # Set this to the server IP and port.
-              endpoint = "glepage.com:51820";
-
-              # Send keepalives every 25 seconds. Important to keep NAT tables alive.
-              persistentKeepalive = 25;
-            }
-          ];
-        };
+            # Send keepalives every 25 seconds. Important to keep NAT tables alive.
+            persistentKeepalive = 25;
+          }
+        ];
       };
     };
 }
