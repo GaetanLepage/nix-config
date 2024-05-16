ffa2f4db60947db7932c19ac49e1f44b86c54ac5
Gaetan Lepage <gaetan@glepage.com>
2024-05-16 11:32:02 +0200

    nixos/common/remote-builders: update public key for darwin builder

--- nixos/common/remote-builders/default.nix
+++ nixos/common/remote-builders/default.nix
@@ -38,7 +38,7 @@
         hostName = "darwin-build-box.nix-community.org";
         sshUser = "glepage";
         sshKey = config.age.secrets.darwin-build-box-ssh-key.path;
-        publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUREbmFWRDlUWkRKS1NwaUpRZzBxWXMwV1VIRnEzVXI1aWptL2tIT1k5MU4K";
+        publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUZ6OEZYU1ZFZGY4RnZETWZib3hoQjVWalNlN3kyV2dTYTA5cTFMNHQwOTkgCg";
         maxJobs = 32;
         systems = [
           "aarch64-darwin"
