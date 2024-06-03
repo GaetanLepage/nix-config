d36770d9ffe4346cd161675ed6db36663f8ed16c
Gaetan Lepage <gaetan@glepage.com>
2024-06-03 22:01:11 +0200

    home/tui/dev/nix: rename nil-ls

--- home/modules/tui/dev/nix/default.nix
+++ home/modules/tui/dev/nix/default.nix
@@ -46,7 +46,7 @@
   };
 
   programs.nixvim = {
-    plugins.lsp.servers.nil_ls.enable = true;
+    plugins.lsp.servers.nil-ls.enable = true;
 
     # Set indentation to 2 spaces
     files."after/ftplugin/nix.lua" = {
