3d0f2831f0e79a7da1a6fd241705629e575b8050
Gaetan Lepage <gaetan@glepage.com>
2024-06-29 14:35:00 +0200

    nixos/framework/variant: use qt5 variant of sddm theme

--- nixos/framework/wayland.nix
+++ nixos/framework/wayland.nix
@@ -21,7 +21,7 @@
         enable = true;
         wayland.enable = true;
         autoNumlock = true;
-        theme = "where_is_my_sddm_theme";
+        theme = "where_is_my_sddm_theme_qt5";
       };
       defaultSession = "sway";
     };
@@ -31,6 +31,7 @@
   };
   environment.systemPackages = [
     (pkgs.where-is-my-sddm-theme.override {
+      variants = ["qt5"];
       themeConfig.General = {
         background = "${pkgs.nixos-artwork.wallpapers.nineish-dark-gray.gnomeFilePath}";
         backgroundMode = "fill";
