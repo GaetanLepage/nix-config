31d3a61662dfcbb74a35212c9882a519252a147f
Gaetan Lepage <gaetan@glepage.com>
2024-09-05 07:45:18 +0200

    Revert "home/tui/dev/nix: refactor review script"
    This reverts commit b802b71a33c68a65bb63961c8dba82cac1798257.

--- home/modules/tui/dev/nix/review.sh
+++ home/modules/tui/dev/nix/review.sh
@@ -6,85 +6,61 @@ arch=${2:-""}
 
 current_dir=$(basename "$PWD")
 if [ "$current_dir" != "nixpkgs" ]; then
-    echo "Not in 'nixpkgs' directory. Aborting."
-    exit 1
+	echo "Not in 'nixpkgs' directory. Aborting."
+	exit 1
 fi
 
 if [ -z "$pr_number" ]; then
-    echo "No PR given. Aborting"
-    exit 1
+	echo "No PR given. Aborting"
+	exit 1
 fi
 
 echo "Running nixpkgs-review for PR #$pr_number"
 
-_mprocs="mprocs --server 127.0.0.1:4050"
+tmux new-session -d -s "$pr_number"
 
-session_name="review"
-if ! tmux has-session -t "$session_name" &>/dev/null; then
-    tmux new-session -d -s "$session_name"
-
-    # start mprocs
-    tmux send-keys -t "$session_name".0 "$_mprocs" C-m
-fi
-
-# Let the mprocs server enough time to start
-sleep 1
+window_id=1
 
 function rev {
-    echo ">>> $1"
-    # command="nixpkgs-review pr --no-shell --post-result --system $1 $pr_number"
-    command="echo $1 PR#$pr_number"
-    name="$pr_number "
-    case $1 in
-    x86_64-linux)
-        name+="linux_x86"
-        ;;
-    aarch64-linux)
-        name+="linux_arm"
-        ;;
-    x86_64-darwin)
-        name+="darwin_x86"
-        ;;
-    aarch64-darwin)
-        name+="darwin_arm"
-        ;;
-    esac
+	echo ">>> $1"
+	tmux new-window -t "$pr_number":"$window_id"
+	tmux rename-window -t "$pr_number":"$window_id" "$1"
 
-    $_mprocs --ctl "{
-        c: batch,
-        cmds: [
-            {c: add-proc, cmd: '$command'},
-            {c: rename-proc, name: '$name'}
-        ]
-    }"
-    sleep 1
+	command="nixpkgs-review pr --no-shell --post-result --system $1 $pr_number"
+	tmux send-keys -t "$pr_number":"$window_id" "$command" C-m
+
+	((window_id++))
+	sleep 1
 }
 
 case $arch in
 all | "")
-    rev x86_64-linux
-    rev aarch64-linux
-    rev x86_64-darwin
-    rev aarch64-darwin
-    ;;
+	rev x86_64-linux
+	rev aarch64-linux
+	rev x86_64-darwin
+	rev aarch64-darwin
+	;;
 
 +(i|a|d|m))
 
-    case $arch in
-    *i*)
-        rev x86_64-linux
-        ;;&
-    *a*)
-        rev aarch64-linux
-        ;;&
-    *d*)
-        rev x86_64-darwin
-        ;;&
-    *m*)
-        rev aarch64-darwin
-        ;;
-    esac
-    ;;
+	case $arch in
+	*i*)
+		rev x86_64-linux
+		;;&
+	*a*)
+		rev aarch64-linux
+		;;&
+	*d*)
+		rev x86_64-darwin
+		;;&
+	*m*)
+		rev aarch64-darwin
+		;;
+	esac
+	;;
 esac
 
-tmux attach-session -t "$session_name"
+# Close window 0
+tmux kill-window -t "$pr_number":0
+
+tmux attach-session -t "$pr_number"
