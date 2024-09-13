e9fd3215dae91790bca06e04879afb162d631006
Gaetan Lepage <gaetan@glepage.com>
2024-09-13 21:04:36 +0200

    home/framework: simplify update script

--- home/hosts/framework/update.nix
+++ home/hosts/framework/update.nix
@@ -14,27 +14,11 @@
         # Update the flake's inputs
         nix flake update $nix_dir -L
 
-        # Reload the system configuration
-        case $1 in
-            cuda)
-                nixos-rebuild switch \
-                    --fast \
-                    --flake $nix_dir\#cuda \
-                    --target-host root@cuda \
-                    --build-host root@cuda
-                ;;
-
-            `hostname`|"")
-                doas nixos-rebuild switch \
-                    --verbose \
-                    --flake $nix_dir \
-                    --builders ""
-
-                # Clean the store (garbage collection)
-                # Also removes old generations
-                # doas nix-collect-garbage --delete-older-than 2d
-                ;;
-        esac
+        doas nixos-rebuild switch \
+            --verbose \
+            --fast \
+            --flake $nix_dir \
+            --builders ""
       '';
   in {
     shellAliases.u = lib.getExe update;
