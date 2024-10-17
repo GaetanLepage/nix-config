ae27c871f1341f08c1ff99822a1cc0f44f1100c7
Gaetan Lepage <gaetan@glepage.com>
2024-10-17 12:57:50 +0200

    nix: remove cuda-maintainers binary cache
    Now included in the nix-community binary cache

--- nixos/common/nix.nix
+++ nixos/common/nix.nix
@@ -63,13 +63,11 @@
 
       substituters = [
         "https://cache.nixos.org"
-        "https://cuda-maintainers.cachix.org"
         "https://nix-community.cachix.org"
       ];
 
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
     };
