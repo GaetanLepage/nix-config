f15682d4827b7f861333e61175e99a1d128931f4
Gaetan Lepage <gaetan@glepage.com>
2024-12-18 17:45:01 +0100

    home/gui/kanshi: refactor

--- home/modules/gui/wayland/kanshi.nix
+++ home/modules/gui/wayland/kanshi.nix
@@ -1,90 +1,143 @@
 {
   lib,
   pkgs,
+  config,
   ...
-}: {
-  services.kanshi = {
-    enable = true;
+}: let
+  inherit (lib) types;
 
-    settings = let
-      laptopScreenName = "eDP-1";
-      disabledLaptopScreen = {
-        criteria = laptopScreenName;
-        status = "disable";
+  cfg = config.services.kanshi;
+in {
+  options = {
+    services.kanshi = let
+      screenType = types.submodule {
+        options =
+          lib.mapAttrs (_: props:
+            lib.mkOption ({
+                inherit (props) type;
+              }
+              // lib.optionalAttrs
+              (props ? default)
+              {
+                inherit (props) default;
+              }))
+          {
+            criteria.type = types.str;
+            status = {
+              type = types.enum ["enable" "disable"];
+              default = "disable";
+            };
+            scale = {
+              type = types.float;
+              default = 1.0;
+            };
+            position = {
+              type = types.str;
+              default = "0,0";
+            };
+            mode.type = types.str;
+          };
       };
-      laptopScreen = {
-        criteria = laptopScreenName;
-        mode = "2256x1504";
-        position = "0,0";
-        scale = 1.333333;
-        status = "enable";
+    in {
+      screens = lib.mkOption {
+        type = types.attrsOf screenType;
       };
 
-      mkWifiHook = on: "${lib.getExe' pkgs.networkmanager "nmcli"} radio wifi ${
-        if on
-        then "on"
-        else "off"
-      }";
+      _profiles = lib.mkOption {
+        type = types.attrsOf (
+          types.submodule {
+            options = {
+              screens = lib.mkOption {
+                type = with types; attrsOf anything;
+              };
+              wifi = lib.mkEnableOption "wifi";
+            };
+          }
+        );
+      };
+    };
+  };
 
-      home = let
-        homeScreenName = "LG Electronics LG ULTRAGEAR 108MAHU2AU49";
-        homeScreen = {
-          criteria = homeScreenName;
+  config = {
+    services.kanshi = {
+      enable = true;
+
+      screens = {
+        laptop = {
+          criteria = "eDP-1";
+          mode = "2256x1504";
+          position = "0,0";
+          scale = 1.333333;
+        };
+        home = {
+          criteria = "LG Electronics LG ULTRAGEAR 108MAHU2AU49";
           mode = "2560x1440@143.932999Hz";
           position = "0,0";
         };
-      in [
-        # home
-        {
-          profile = {
-            name = "home";
-            outputs = [
-              disabledLaptopScreen
-              homeScreen
-            ];
-            exec = mkWifiHook false;
-          };
-        }
+        inria = {
+          criteria = "Dell Inc. DELL U2719DC 88QSTS2";
+          position = "0,0";
+          mode = "2560x1440@59.95Hz";
+        };
+      };
 
-        # stream
-        {
-          profile = {
-            name = "stream";
-            outputs = [
-              (laptopScreen // {position = "0,310";})
-              (homeScreen // {position = "1694,0";})
-            ];
-            exec = mkWifiHook false;
+      _profiles = {
+        laptop = {
+          screens.laptop.status = "enable";
+          wifi = true;
+        };
+        home = {
+          wifi = false;
+          screens = {
+            laptop.status = "disable";
+            home.status = "enable";
           };
-        }
-      ];
-    in
-      [
-        # laptop
-        {
-          profile = {
-            name = "laptop";
-            outputs = [laptopScreen];
-            exec = mkWifiHook true;
+        };
+        stream = {
+          inherit (cfg._profiles.home) wifi;
+          screens = {
+            laptop = {
+              status = "enable";
+              position = "0,310";
+            };
+            home = {
+              status = "enable";
+              position = "1694,0";
+            };
           };
-        }
+        };
+        inria = {
+          screens = {
+            laptop.status = "disable";
+            inria.status = "enable";
+          };
+          wifi = true;
+        };
+      };
 
-        # inria
-        {
+      settings = let
+        mkWifiHook = on: "${lib.getExe' pkgs.networkmanager "nmcli"} radio wifi ${
+          if on
+          then "on"
+          else "off"
+        }";
+      in
+        lib.mapAttrsToList (name: props: {
           profile = {
-            name = "inria";
-            outputs = [
-              disabledLaptopScreen
-              {
-                criteria = "Dell Inc. DELL U2719DC 88QSTS2";
-                position = "0,0";
-                mode = "2560x1440@59.95Hz";
-              }
-            ];
-            exec = mkWifiHook true;
+            inherit name;
+            exec = mkWifiHook props.wifi;
+            outputs =
+              lib.mapAttrsToList (
+                name: props: let
+                  defaults = cfg.screens.${name};
+                  overrides = lib.filterAttrs (_: v: v != null) props;
+                in
+                  defaults // overrides
+              )
+              props.screens;
           };
-        }
-      ]
-      ++ home;
+        })
+        cfg._profiles;
+    };
   };
 }
