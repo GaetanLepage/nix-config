0eac68fa668f9cc2c245c9b0692ad0ab02555e48
Gaetan Lepage <gaetan@glepage.com>
2025-01-03 19:06:30 +0100

    home/gui/streaming: use nixpkgs obs-do package

--- home/modules/gui/wayland/streaming.nix
+++ home/modules/gui/wayland/streaming.nix
@@ -24,23 +24,8 @@
   wayland.windowManager.sway.config.keybindings = {
     F6 =
       let
-        obs-do = lib.getExe (
-          pkgs.obs-do.overrideAttrs (old: rec {
-            version = "0.1.6";
-            src = pkgs.fetchFromGitHub {
-              owner = "jonhoo";
-              repo = "obs-do";
-              tag = "v0.1.6";
-              hash = "sha256-t6m/PX4GMCFH9wFrOaU/dcrbKitUXQlOcU7aUyJPpxA=";
-            };
-            cargoDeps = old.cargoDeps.overrideAttrs (
-              lib.const {
-                inherit src;
-                outputHash = "sha256-ggxNVgtSAU8tr4mPUvhXIqq02v47y+arMUbk0zlc5yY=";
-              }
-            );
-          })
-        );
+        obs-do = lib.getExe pkgs.obs-do;
+
         toggle-script = pkgs.writeShellScript "mute-obs" ''
           if ${obs-do} toggle-mute; then
             if ${obs-do} get-mute | grep true; then
