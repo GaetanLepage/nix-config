9cdc1df28814d49208a8833364b502e04e10299d
Gaetan Lepage <gaetan@glepage.com>
2025-01-09 00:03:08 +0100

    home/gui/email: add dev@glepage.com and refactor module

--- home/modules/gui/email.nix
+++ home/modules/gui/email.nix
@@ -9,9 +9,9 @@
   };
 
   accounts.email.accounts =
-    lib.mapAttrs
-      (
-        _: config:
+    let
+      applyDefaults =
+        config:
         {
           realName = "Gaetan Lepage";
           userName = config.address;
@@ -31,77 +31,84 @@
             };
           };
         }
-        // config
-      )
-      {
-        perso = {
-          address = "gaetan@glepage.com";
+        // config;
 
-          primary = true;
-
-          imap = {
-            host = "mail.glepage.com";
-            port = 143;
-            tls.useStartTls = true;
-          };
-
-          smtp = {
-            host = "mail.glepage.com";
-            port = 587;
-            tls.useStartTls = true;
-          };
+      # Defaults for all of my @glepage.com accounts
+      glepageDefaults = {
+        imap = {
+          host = "mail.glepage.com";
+          port = 143;
+          tls.useStartTls = true;
         };
 
-        # inria
-        inria = {
-          address = "gaetan.lepage@inria.fr";
-          userName = "galepage";
-
-          imap = {
-            host = "zimbra.inria.fr";
-            port = 993;
-          };
-
-          smtp = {
-            host = "smtp.inria.fr";
-            port = 587;
-            tls.useStartTls = true;
-          };
-        };
-
-        gmx = {
-          address = "g.lepage@gmx.com";
-
-          imap = {
-            host = "imap.gmx.com";
-            port = 993;
-          };
-
-          smtp = {
-            host = "mail.gmx.com";
-            port = 465;
-          };
-        };
-
-        hotmail = {
-          address = "gaetlep@hotmail.fr";
-          flavor = "outlook.office365.com";
-        };
-
-        # inria
-        uga = {
-          address = "gaetan.lepage@univ-grenoble-alpes.fr";
-
-          imap = {
-            host = "zimbra.univ-grenoble-alpes.fr";
-            port = 993;
-          };
-
-          smtp = {
-            host = "smtps.univ-grenoble-alpes.fr";
-            port = 587;
-            tls.useStartTls = true;
-          };
+        smtp = {
+          host = "mail.glepage.com";
+          port = 587;
+          tls.useStartTls = true;
         };
       };
+    in
+    lib.mapAttrs (_: applyDefaults) {
+      perso = {
+        address = "gaetan@glepage.com";
+
+        primary = true;
+      } // glepageDefaults;
+
+      dev = {
+        address = "dev@glepage.com";
+      } // glepageDefaults;
+
+      # inria
+      inria = {
+        address = "gaetan.lepage@inria.fr";
+        userName = "galepage";
+
+        imap = {
+          host = "zimbra.inria.fr";
+          port = 993;
+        };
+
+        smtp = {
+          host = "smtp.inria.fr";
+          port = 587;
+          tls.useStartTls = true;
+        };
+      };
+
+      gmx = {
+        address = "g.lepage@gmx.com";
+
+        imap = {
+          host = "imap.gmx.com";
+          port = 993;
+        };
+
+        smtp = {
+          host = "mail.gmx.com";
+          port = 465;
+        };
+      };
+
+      hotmail = {
+        address = "gaetlep@hotmail.fr";
+        flavor = "outlook.office365.com";
+      };
+
+      # inria
+      uga = {
+        address = "gaetan.lepage@univ-grenoble-alpes.fr";
+
+        imap = {
+          host = "zimbra.univ-grenoble-alpes.fr";
+          port = 993;
+        };
+
+        smtp = {
+          host = "smtps.univ-grenoble-alpes.fr";
+          port = 587;
+          tls.useStartTls = true;
+        };
+      };
+    };
 }
