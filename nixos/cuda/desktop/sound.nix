caf75fbe9da5c79c26b10522a30ff2a7724303db
Gaetan Lepage <gaetan@glepage.com>
2025-01-13 11:21:17 +0100

    nixos/cuda: remove useless code

--- nixos/cuda/desktop/sound.nix
+++ nixos/cuda/desktop/sound.nix
@@ -3,14 +3,6 @@
 
   services.pipewire = {
     enable = true;
-
     pulse.enable = true;
-
-    alsa = {
-      enable = true;
-      support32Bit = true;
-    };
   };
-
-  nixpkgs.config.pulseaudio = true;
 }
