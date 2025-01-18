d811c5ff680a795b263139dbaff281a57b3f4c79
Gaetan Lepage <gaetan@glepage.com>
2025-01-18 11:06:51 +0100

    home: move keyring config to common gui module

--- home/hosts/framework/default.nix
+++ home/hosts/framework/default.nix
@@ -7,10 +7,6 @@
     ./backup
   ];
 
-  # Allows auto-unlocking of the gnome keyring at login
-  services.gnome-keyring.enable = true;
-  home.sessionVariables.SSH_AUTH_SOCK = "$XDG_RUNTIME_DIR/keyring/ssh";
-
   # Load the pulseaudio module that enables sharing audio devices with computers on the network.
   # inria
   programs.fish.interactiveShellInit = ''
--- home/modules/gui/default.nix
+++ home/modules/gui/default.nix
@@ -12,6 +12,10 @@
     ./zathura.nix
   ];
 
+  # Allows auto-unlocking of the gnome keyring at login
+  services.gnome-keyring.enable = true;
+  home.sessionVariables.SSH_AUTH_SOCK = "$XDG_RUNTIME_DIR/keyring/ssh";
+
   home.packages = with pkgs; [
     libnotify
 
