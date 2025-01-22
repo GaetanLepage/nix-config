5b95e63ab20584f1b8c9e4aff6df7220c1e2dee6
Gaetan Lepage <gaetan@glepage.com>
2025-01-22 08:15:06 +0100

    Revert "[TO-REVERT] temporary overlay for tree-sitter"
    This reverts commit 312419107a1c5bedb25b47d85aec5eb62362bca9.

--- flake/nixos.nix
+++ flake/nixos.nix
@@ -7,22 +7,6 @@
         inputs.nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           specialArgs.inputs = inputs;
-          ####################################################################
-          specialArgs.pkgs =
-            let
-              system = "x86_64-linux";
-              ts-nixpkgs = import inputs.nixpkgs-tree-sitter { system = "x86_64-linux"; };
-              ts-overlay = final: prev: { inherit (ts-nixpkgs) tree-sitter; };
-            in
-            import inputs.nixpkgs {
-              inherit system;
-              config = {
-                allowUnfree = true;
-                permittedInsecurePackages = [ "olm-3.2.16" ];
-              };
-              overlays = [ ts-overlay ];
-            };
-          ####################################################################
           modules = [
             # The system configuration
             ../nixos/${hostname}
