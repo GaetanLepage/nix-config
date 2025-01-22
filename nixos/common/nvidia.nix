2152fb96601e214f77d3fdd45fb50dcecb7b3edd
Gaetan Lepage <gaetan@glepage.com>
2025-01-22 10:28:29 +0100

    nixos/nvidia: use open kernel modules

--- nixos/common/nvidia.nix
+++ nixos/common/nvidia.nix
@@ -41,14 +41,8 @@
           # Experimental and only works on modern Nvidia GPUs (Turing or newer).
           powerManagement.finegrained = false;
 
-          # Use the NVidia open source kernel module (not to be confused with the
-          # independent third-party "nouveau" open source driver).
-          # Support is limited to the Turing and later architectures. Full list of
-          # supported GPUs is at:
-          # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
-          # Only available from driver 515.43.04+
-          # Currently alpha-quality/buggy, so false is currently the recommended setting.
-          open = false;
+          # Use the NVidia open source kernel module
+          open = true;
 
           # Enable the Nvidia settings menu,
           # accessible via `nvidia-settings`.
