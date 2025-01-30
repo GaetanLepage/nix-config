30616ffcde666a1f498eb31381c12253b0d2b983
Gaetan Lepage <gaetan@glepage.com>
2025-01-30 09:01:36 +0100

    nixos/common/wireguard: implement redirectAllTraffic

--- nixos/common/wireguard.nix
+++ nixos/common/wireguard.nix
@@ -10,39 +10,68 @@
     ip = lib.mkOption {
       type = lib.types.str;
     };
+
+    redirectAllTraffic = lib.mkEnableOption "";
+
+    allowedIPs = lib.mkOption {
+      type = lib.types.str;
+    };
   };
 
   config =
     let
       cfg = config.my-modules.wireguard;
-    in
-    lib.mkIf cfg.enable {
-      networking = {
-        # Use my own DNS server.
-        # Using `networkmanager.insertNameservers` instead od `nameservers` ensures this server is added
-        # first and takes the priority over other DNS servers.
-        networkmanager.insertNameservers = [ "10.10.10.1" ];
 
-        wireguard.interfaces.wg0 = {
-          ips = [ "${cfg.ip}/32" ];
-          listenPort = 51820;
-          privateKeyFile = config.age.secrets.wireguard-private-key.path;
+      localIp = "${cfg.ip}/32";
+      dnsIp = "10.10.10.1";
 
-          peers = [
-            {
-              endpoint = "vpn.glepage.com:51820";
-              publicKey = "jWzlVwkNkaO1uj7Qh+Xemo0EtxIYP2ufK+18oPcdvBY=";
+      commonConfig = {
+        listenPort = 51820;
+        privateKeyFile = config.age.secrets.wireguard-private-key.path;
 
-              # Forward all the traffic via VPN.
-              #allowedIPs = [ "0.0.0.0/0" ];
-              # Or forward only particular subnets
-              allowedIPs = [ "10.10.10.0/24" ];
+        peers = [
+          {
+            endpoint = "162.19.67.38:51820";
+            publicKey = "jWzlVwkNkaO1uj7Qh+Xemo0EtxIYP2ufK+18oPcdvBY=";
 
-              # Send keepalives every 25 seconds. Important to keep NAT tables alive.
-              persistentKeepalive = 25;
-            }
-          ];
-        };
+            # Forward all the traffic via VPN.
+            #allowedIPs = [ "0.0.0.0/0" ];
+            # Or forward only particular subnets
+            allowedIPs = [ cfg.allowedIPs ];
+
+            # Send keepalives every 25 seconds. Important to keep NAT tables alive.
+            persistentKeepalive = 25;
+          }
+        ];
       };
-    };
+    in
+    lib.mkIf cfg.enable (
+      lib.mkMerge [
+        (lib.mkIf cfg.redirectAllTraffic {
+
+          my-modules.wireguard.allowedIPs = lib.mkDefault "0.0.0.0/0";
+
+          networking.wg-quick.interfaces.wg0 = commonConfig // {
+            address = [ localIp ];
+            dns = [ dnsIp ];
+            autostart = true;
+          };
+        })
+        (lib.mkIf (!cfg.redirectAllTraffic) {
+
+          my-modules.wireguard.allowedIPs = lib.mkDefault "10.10.10.0/24";
+
+          networking = {
+            # Use my own DNS server.
+            # Using `networkmanager.insertNameservers` instead od `nameservers` ensures this server is added
+            # first and takes the priority over other DNS servers.
+            networkmanager.insertNameservers = [ dnsIp ];
+
+            wireguard.interfaces.wg0 = commonConfig // {
+              ips = [ localIp ];
+            };
+          };
+        })
+      ]
+    );
 }
