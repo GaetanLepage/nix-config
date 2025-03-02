96edcbccfcdb763868ab8c84f4427656fc2f014e
Gaetan Lepage <gaetan@glepage.com>
2025-03-02 21:28:47 +0100

    home/framework/backup: format script

--- home/hosts/framework/backup/script.sh
+++ home/hosts/framework/backup/script.sh
@@ -2,35 +2,35 @@ notify-send "Starting backup"
 
 DRY_RUN=''
 if [ "${1-}" = "-d" ]; then
-	DRY_RUN='--dry-run'
+    DRY_RUN='--dry-run'
 fi
 
 dest_path=/tank/gaetan/backup/homedir
 
 rsync -zravut "$HOME/" server:$dest_path \
-	--human-readable \
-	--delete \
-	--delete-excluded \
-	--exclude-from=@excludes@ \
-	$DRY_RUN
+    --human-readable \
+    --delete \
+    --delete-excluded \
+    --exclude-from=@excludes@ \
+    $DRY_RUN
 
 rsync_exit_code=$?
 
 # Handle exit code 24 (i.e. 'Partial transfer due to vanished source files')
 case $rsync_exit_code in
 0 | 24)
-	success_message="Backup succesfull"
-	echo "=> $success_message"
-	notify-send "$success_message"
+    success_message="Backup succesfull"
+    echo "=> $success_message"
+    notify-send "$success_message"
 
-	exit 0
-	;;
+    exit 0
+    ;;
 
 *)
-	error_message="Backup failed with error $rsync_exit_code"
-	echo "=> $error_message"
-	notify-send "$error_message"
+    error_message="Backup failed with error $rsync_exit_code"
+    echo "=> $error_message"
+    notify-send "$error_message"
 
-	exit 1
-	;;
+    exit 1
+    ;;
 esac
