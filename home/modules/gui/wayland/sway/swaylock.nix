bff87aa1a52212c6bc4901c0de30efe860da524a
Gaetan Lepage <gaetan@glepage.com>
2025-03-12 14:56:02 +0100

    home/gui/swaylock: stop using string interpolation for the wallpaper's path

--- home/modules/gui/wayland/sway/swaylock.nix
+++ home/modules/gui/wayland/sway/swaylock.nix
@@ -5,7 +5,7 @@
     settings = {
       daemonize = true;
 
-      image = builtins.toString ../wallpaper.png;
+      image = ../wallpaper.png;
       indicator-x-position = 100;
       indicator-y-position = 100;
     };
