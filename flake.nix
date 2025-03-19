0541e5e1658c8180111e0f07f9aada689f46c2dd
Gaetan Lepage <gaetan@glepage.com>
2025-03-19 08:21:16 +0100

    home/tui/neovim: useGlobalPackages for nixvim

--- flake.nix
+++ flake.nix
@@ -54,7 +54,7 @@
 
     nixvim = {
       url = "github:nix-community/nixvim";
-      # inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     nix-index-database = {
