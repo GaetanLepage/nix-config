c3264e4b4a952d165a144cd587d32259bd24e5e5
Gaetan Lepage <gaetan@glepage.com>
2025-04-29 11:51:57 +0200

    flake: move systems to flake/default.nix

--- flake.nix
+++ flake.nix
@@ -74,13 +74,6 @@
       ...
     }@inputs:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-
       imports = [ ./flake ];
     };
 }
--- flake/default.nix
+++ flake/default.nix
@@ -9,4 +9,11 @@
     ./substituters.nix
     ./treefmt.nix
   ];
+
+  systems = [
+    "x86_64-linux"
+    "aarch64-linux"
+    "x86_64-darwin"
+    "aarch64-darwin"
+  ];
 }
