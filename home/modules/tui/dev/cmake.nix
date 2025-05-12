1c6e0b580ca7f2e40c3996f8c8a7696ee7c2659a
Gaetan Lepage <gaetan@glepage.com>
2025-05-13 00:28:56 +0200

    home/tui/dev/cmake: disable auto-format for cmake files

--- home/modules/tui/dev/cmake.nix
+++ home/modules/tui/dev/cmake.nix
@@ -9,27 +9,9 @@
   ];
 
   programs.nixvim = {
-    plugins.lsp.servers.cmake = {
-      enable = true;
-      package =
-        let
-          formatConfigFile = pkgs.writers.writeJSON "cmake-format-config.json" {
-            format.dangle_parens = true;
-          };
-        in
-        pkgs.cmake-language-server.overrideAttrs (old: {
-          postPatch =
-            ''
-              substituteInPlace cmake_language_server/server.py \
-                --replace-fail \
-                  '["cmake-format",' \
-                  '["cmake-format", "--dangle-parens", "true",'
-            ''
-            # '["cmake-format", "--config-file", "${formatConfigFile}",'
-            + (old.postPatch or "");
-
-          disabledTests = [ "test_formatting" ] ++ (old.disabledTests or [ ]);
-        });
+    plugins = {
+      lsp-format.settings.cmake.exclude = [ "cmake" ];
+      lsp.servers.cmake.enable = true;
     };
   };
 }
