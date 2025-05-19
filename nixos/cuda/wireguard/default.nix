49d9ec35e1f24e87274dc12ad0636f1a6b481c8e
Gaetan Lepage <gaetan@glepage.com>
2025-05-19 15:02:07 +0200

    nixos/cuda: add mullvad wireguard config (commented)

--- nixos/cuda/wireguard/default.nix
+++ nixos/cuda/wireguard/default.nix
@@ -5,4 +5,25 @@
     enable = true;
     ip = "10.10.10.5";
   };
+  # services.resolved.enable = true;
+  # networking.wg-quick.interfaces.mullvad = {
+  #   address = [ "10.74.39.28/32,fc00:bbbb:bbbb:bb01::b:271b/128" ];
+  #   dns = [ "10.64.0.1" ];
+  #   autostart = true;
+  #   # listenPort = 51820;
+  #   privateKeyFile = "/home/gaetan/temp/privatekey";
+  #
+  #   peers = [
+  #     {
+  #       endpoint = "193.32.127.67:51820";
+  #       publicKey = "qcvI02LwBnTb7aFrOyZSWvg4kb7zNW9/+rS6alnWyFE=";
+  #
+  #       allowedIPs = [ "0.0.0.0/0,::0/0" ];
+  #
+  #       # Send keepalives every 25 seconds. Important to keep NAT tables alive.
+  #       persistentKeepalive = 25;
+  #     }
+  #   ];
+  # };
+
 }
