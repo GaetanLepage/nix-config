1d7fceca6b3854f5d1512c02647e1379ffd0fb03
Gaetan Lepage <gaetan@glepage.com>
2025-05-20 22:46:59 +0200

    home/gui/nheko: use settings

--- home/modules/gui/nheko.nix
+++ home/modules/gui/nheko.nix
@@ -1,17 +1,5 @@
 {
   programs.nheko = {
     enable = true;
-
-    settings = {
-      auth = {
-        home_server = "https://matrix-client.matrix.org:443";
-        user_id = "@@glepage:matrix.org";
-      };
-      user = {
-        "window\\start_in_tray" = false;
-        "window\\tray" = false;
-        small_avatars_enabled = false;
-      };
-    };
   };
 }
