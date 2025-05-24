f02920bbcd61df9ddf15f4af5f265235dfad85fb
Gaetan Lepage <gaetan@glepage.com>
2025-05-24 17:34:53 +0200

    home/inria: move to modules

--- flake/home-manager.nix
+++ flake/home-manager.nix
@@ -1,19 +1,6 @@
 { inputs, ... }:
 {
   flake.homeConfigurations = {
-    # inria
-    inria = inputs.home-manager.lib.homeManagerConfiguration {
-      pkgs = import inputs.nixpkgs {
-        system = "x86_64-linux";
-        config = {
-          allowUnfree = true;
-          cudaSupport = true;
-        };
-      };
-      extraSpecialArgs.inputs = inputs;
-      modules = [ ../home/hosts/inria ];
-    };
-
     jrs = inputs.home-manager.lib.homeManagerConfiguration {
       pkgs = import inputs.nixpkgs {
         system = "x86_64-linux";
