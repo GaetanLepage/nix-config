1b645bdc8b13760baa944c2eb51c75f88260162a
Gaetan Lepage <gaetan@glepage.com>
2025-05-26 01:34:11 +0200

    modules/home/desktop: move imports to dedicated file

---
+++ modules/home/desktop/imports.nix
@@ -0,0 +1,11 @@
+{ config, ... }:
+{
+  flake.modules.homeManager.desktop.imports = with config.flake.modules.homeManager; [
+    dunst
+    email
+    gtk
+    kanshi
+    wayland
+    zathura
+  ];
+}
