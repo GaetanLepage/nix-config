d99f2164715c442d7ce06286d6fa268960d08b26
Gaetan Lepage <gaetan@glepage.com>
2025-05-26 01:35:23 +0200

    modules/home/core: move imports to dedicated file

--- modules/home/core/default.nix
+++ modules/home/core/default.nix
@@ -1,14 +1,7 @@
-{ config, ... }:
 {
   flake.modules.homeManager.core =
     { lib, ... }:
     {
-      imports = with config.flake.modules.homeManager; [
-        homeManager
-        nix-index-database
-        substituters
-      ];
-
       home = {
         username = lib.mkDefault "gaetan"; # inria (remove mkDefault)
         homeDirectory = lib.mkDefault "/home/gaetan"; # inria
