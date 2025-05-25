f865bd3a6b1c15a120735ae7a9d89f4cab5c3e94
Gaetan Lepage <gaetan@glepage.com>
2025-05-25 23:28:59 +0200

    home/tui: move homeDirectory and username to modules/home/core

--- modules/home/core/default.nix
+++ modules/home/core/default.nix
@@ -1,10 +1,17 @@
 { config, ... }:
 {
-  flake.modules.homeManager.core = {
-    imports = with config.flake.modules.homeManager; [
-      homeManager
-      nix-index-database
-      substituters
-    ];
-  };
+  flake.modules.homeManager.core =
+    { lib, ... }:
+    {
+      imports = with config.flake.modules.homeManager; [
+        homeManager
+        nix-index-database
+        substituters
+      ];
+
+      home = {
+        username = lib.mkDefault "gaetan"; # inria (remove mkDefault)
+        homeDirectory = lib.mkDefault "/home/gaetan"; # inria
+      };
+    };
 }
