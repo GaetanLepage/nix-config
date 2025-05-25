405d0f1497e53e86315835099a0e8158d6c71a39
Gaetan Lepage <gaetan@glepage.com>
2025-05-26 01:10:46 +0200

    home/gui/wayland: move default things to modules

--- home/modules/gui/wayland/default.nix
+++ home/modules/gui/wayland/default.nix
@@ -1,32 +1,8 @@
 {
-  pkgs,
-  ...
-}:
-{
   imports = [
     ./gammastep.nix
     ./streaming.nix
     ./sway
     ./waybar
   ];
-
-  config = {
-    home = {
-      packages = with pkgs; [
-        wdisplays
-        wlr-randr
-        wl-clipboard
-      ];
-
-      sessionVariables = {
-        NIXOS_OZONE_WL = 1;
-
-        SDL_VIDEODRIVER = "wayland";
-        QT_QPA_PLATFORM = "wayland";
-        QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
-        _JAVA_AWT_WM_NONREPARENTING = "1";
-        XDG_SESSION_TYPE = "wayland";
-      };
-    };
-  };
 }
