28e8e8903d35f7553513e791039c2c95b00e64e1
Gaetan Lepage <gaetan@glepage.com>
2025-05-26 11:57:43 +0200

    home/modules/gui: move sway to modules

---
+++ modules/home/desktop/wayland/swaylock.nix
@@ -0,0 +1,15 @@
+{
+  flake.modules.homeManager.swaylock = {
+    programs.swaylock = {
+      enable = true;
+
+      settings = {
+        daemonize = true;
+
+        image = ./wallpaper.png;
+        indicator-x-position = 100;
+        indicator-y-position = 100;
+      };
+    };
+  };
+}
