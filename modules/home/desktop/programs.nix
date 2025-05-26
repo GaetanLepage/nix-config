32e4e6cca221a06ff898e86fbeed48efaee573f4
Gaetan Lepage <gaetan@glepage.com>
2025-05-26 11:26:35 +0200

    home/modules/gui: move programs to modules

---
+++ modules/home/desktop/programs.nix
@@ -0,0 +1,24 @@
+{
+  flake.modules.homeManager.desktop-programs =
+    { pkgs, ... }:
+    {
+      home.packages = with pkgs; [
+        ## GUI applications
+        inkscape # inria
+        drawio # inria
+
+        # IM
+        discord
+        signal-desktop
+        slack # inria
+
+        # Office / productivity
+        libreoffice-fresh
+        kdePackages.okular # inria ? (also delete keyboard shortcuts file)
+        zotero # inria
+
+        # Misc
+        spotify
+      ];
+    };
+}
