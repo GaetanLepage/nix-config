9eca4c159e48dcc1ef81a80edff8b752a7d3c56f
Gaetan Lepage <gaetan@glepage.com>
2025-05-26 16:39:24 +0200

    nixos/_modules: move to modules/nixos/core

--- modules/nixos/core/imports.nix
+++ modules/nixos/core/imports.nix
@@ -1,7 +1,14 @@
-{ config, ... }:
+{ config, inputs, ... }:
 {
   flake.modules.nixos.core.imports = with config.flake.modules.nixos; [
+    inputs.disko.nixosModules.disko
+
     agenix
+    bootloader
     nix
+    security
+    ssh-server
+    users
+    wireguard-client
   ];
 }
