6b1b0f06aee25ee1b6feb1c0e56015c4f37a4468
Gaetan Lepage <gaetan@glepage.com>
2025-05-27 14:00:35 +0200

    nixos/framework/sound: remove unneeded pavucontrol and pulseaudio

--- nixos/framework/sound.nix
+++ nixos/framework/sound.nix
@@ -1,12 +1,4 @@
-{ pkgs, ... }:
 {
-  environment.systemPackages = with pkgs; [
-    pavucontrol
-
-    # Even though using Pipewire, pulseaudio provides `pactl`
-    pulseaudio
-  ];
-
   services.pipewire = {
     enable = true;
     pulse.enable = true;
