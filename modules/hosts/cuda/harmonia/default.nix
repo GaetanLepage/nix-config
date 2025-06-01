2ec9a57d5eb7503e932529aa3494f4903ceeac04
Gaetan Lepage <gaetan@glepage.com>
2025-06-01 14:41:04 +0200

    nixos/cuda: migrate to modules

---
+++ modules/hosts/cuda/harmonia/default.nix
@@ -0,0 +1,25 @@
+{
+  flake.modules.nixos.host_cuda =
+    { config, ... }:
+    let
+      port = 5000;
+    in
+    {
+      # Public and private keys
+      # Generated with: `nix-store --generate-binary-cache-key cache.glepage.com private public`
+      # Public: cache.glepage.com:iaI4fkpeSy8sg8u5M8KnMsl4mSS1vwMYWj+ipqz9ugg=
+      age.secrets.harmonia-private-key.rekeyFile = ./harmonia-private-key.age;
+
+      services.harmonia = {
+        enable = true;
+
+        signKeyPaths = [
+          config.age.secrets.harmonia-private-key.path
+        ];
+
+        settings.bind = "${config.wireguardClient.ip}:${toString port}";
+      };
+
+      networking.firewall.allowedTCPPorts = [ port ];
+    };
+}
