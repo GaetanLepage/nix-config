2142bf2cae92da41bfaeb2e7679b5c9c64f0d465
Gaetan Lepage <gaetan@glepage.com>
2025-07-11 01:04:52 +0200

    framework: enable fingerprint login + autologin

---
+++ modules/hosts/framework/login.nix
@@ -0,0 +1,30 @@
+{
+  flake.modules.nixos.host_framework =
+    { config, ... }:
+    {
+
+      services = {
+        # Enable fingerprint sensor
+        fprintd.enable = true;
+      };
+
+      # Enable fingerprint authentification for swaylock
+      security.pam.services.swaylock = {
+        enable = true;
+
+        # Using `fprintAuth = true` does not allow password-only unlocking
+        text = ''
+          auth sufficient pam_unix.so
+          auth sufficient ${config.services.fprintd.package}/lib/security/pam_fprintd.so
+          auth required pam_deny.so
+        '';
+      };
+
+      # Disable fingerprint authentification for SDDM (display manager)
+      # If enable, both password AND fingerprints are required
+      security.pam.services.login.fprintAuth = false;
+
+      # Set up auto login (disk encryption password must be provided at startup anyway)
+      services.displayManager.autoLogin.user = "gaetan";
+    };
+}
