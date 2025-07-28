c6124d22686f964675b6975df3de8fd10e44d229
Gaetan Lepage <gaetan@glepage.com>
2025-07-28 15:36:16 +0200

    hosts/cuda/home/kanshi: add stream profile

--- modules/hosts/cuda/home.nix
+++ modules/hosts/cuda/home.nix
@@ -10,7 +10,24 @@
 
     services.kanshi._profiles = {
       main = {
-        screens.lg_27.status = "enable";
+        screens = {
+          lg_27.status = "enable";
+          benq.status = "disable";
+        };
+        wifi = false;
+      };
+      stream = {
+        screens = {
+          lg_27 = {
+            status = "enable";
+            position = "0,247";
+          };
+          benq = {
+            status = "enable";
+            position = "2560,0";
+            transform = "90";
+          };
+        };
         wifi = false;
       };
     };
