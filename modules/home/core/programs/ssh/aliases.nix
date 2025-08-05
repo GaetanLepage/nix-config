8604db6a52bbddd6a463e084d113cb3c56f62afd
Gaetan Lepage <gaetan@glepage.com>
2025-08-05 16:00:58 +0200

    home/ssh/aliases: remove alias for "ssh alya"

--- modules/home/core/programs/ssh/aliases.nix
+++ modules/home/core/programs/ssh/aliases.nix
@@ -2,7 +2,6 @@
   flake.modules.homeManager.core = {
     home.shellAliases = {
       s = "ssh";
-      sa = "ssh alya"; # inria
       sc = "ssh cuda";
       sca = "ssh cuda -t zellij attach --create main";
       sj = "ssh jrs";
