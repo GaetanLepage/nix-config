ad9211b28c87f24c826f4830db3341d1ccd304cd
Gaetan Lepage <gaetan@glepage.com>
2025-08-19 18:11:03 +0200

    nixos/desktop/mullvad: init

---
+++ modules/nixos/desktop/mullvad/default.nix
@@ -0,0 +1,26 @@
+{
+  flake.modules.nixos.desktop =
+    { config, ... }:
+    {
+      age.secrets.mullvad-private-key.rekeyFile = ./private-key.age;
+      networking.wg-quick.interfaces.mullvad = {
+        address = [ "10.74.225.36/32,fc00:bbbb:bbbb:bb01::b:e123/128" ];
+        dns = [ "10.64.0.1" ];
+        autostart = false;
+        listenPort = 51820;
+        privateKeyFile = config.age.secrets.mullvad-private-key.path;
+
+        peers = [
+          {
+            endpoint = "193.32.127.66:51820";
+            publicKey = "/iivwlyqWqxQ0BVWmJRhcXIFdJeo0WbHQ/hZwuXaN3g=";
+
+            allowedIPs = [ "0.0.0.0/0,::0/0" ];
+
+            # Send keepalives every 25 seconds. Important to keep NAT tables alive.
+            persistentKeepalive = 25;
+          }
+        ];
+      };
+    };
+}
