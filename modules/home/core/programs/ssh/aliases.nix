1d3c503ae990bb0484a750aa1b3d22e8adc4274f
Gaetan Lepage <gaetan@glepage.com>
2025-08-26 01:41:43 +0200

    home/core/ssh: add aliases for ssh paris

--- modules/home/core/programs/ssh/aliases.nix
+++ modules/home/core/programs/ssh/aliases.nix
@@ -5,6 +5,8 @@
       sc = "ssh cuda";
       sca = "ssh cuda -t zellij attach --create main";
       sj = "ssh jrs";
+      sp = "ssh paris";
+      spa = "ssh paris -t zellij attach --create main";
     };
   };
 }
