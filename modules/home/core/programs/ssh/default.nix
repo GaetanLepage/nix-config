983de7e59617a965396bc3a505f63c4dfce7b5c9
Gaetan Lepage <gaetan@glepage.com>
2025-08-29 17:47:26 +0200

    home/core/ssh: explicitly declare SSH default config as it will be deprecated in HM

--- modules/home/core/programs/ssh/default.nix
+++ modules/home/core/programs/ssh/default.nix
@@ -16,70 +16,88 @@
       programs.ssh = {
         enable = true;
 
-        matchBlocks =
-          lib.mapAttrs
-            (
-              _: v:
-              {
-                user = "gaetan";
-                identityFile = sshKeysPathPrefix + "perso";
-              }
-              // v
-            )
+        # Explicitly declare default config and disable HM's vendored one.
+        # This will be deprecated after some time.
+        enableDefaultConfig = false;
+
+        matchBlocks = {
+          # Sane settings
+          "*" = {
+            forwardAgent = false;
+            addKeysToAgent = "no";
+            compression = false;
+            serverAliveInterval = 0;
+            serverAliveCountMax = 3;
+            hashKnownHosts = false;
+            userKnownHostsFile = "~/.ssh/known_hosts";
+            controlMaster = "no";
+            controlPath = "~/.ssh/master-%r@%n:%p";
+            controlPersist = "no";
+          };
+        }
+        // (lib.mapAttrs
+          (
+            _: v:
             {
-              ########
-              # Home #
-              ########
+              user = "gaetan";
+              identityFile = sshKeysPathPrefix + "perso";
+            }
+            // v
+          )
+          {
+            ########
+            # Home #
+            ########
 
-              framework.hostname = "10.10.10.2";
-              cuda.hostname = "10.10.10.5";
-              paris.hostname = "10.10.10.4";
-              router = {
-                hostname = "192.168.1.1";
-                user = "root";
-                extraOptions = {
-                  HostKeyAlgorithms = "+ssh-rsa";
-                  PubkeyAcceptedKeyTypes = "+ssh-rsa";
-                };
+            framework.hostname = "10.10.10.2";
+            cuda.hostname = "10.10.10.5";
+            paris.hostname = "10.10.10.4";
+            router = {
+              hostname = "192.168.1.1";
+              user = "root";
+              extraOptions = {
+                HostKeyAlgorithms = "+ssh-rsa";
+                PubkeyAcceptedKeyTypes = "+ssh-rsa";
               };
-              tank = {
-                hostname = "tank.glepage.com";
-                port = 222;
-              };
-              borg = {
-                hostname = "zh5071.rsync.net";
-                user = "zh5071";
-              };
-
-              vps.hostname = "vps.glepage.com";
-
-              pixel = {
-                hostname = "10.10.10.3";
-                port = 2222;
-              };
-
-              "github.com".user = "git";
-              "gitlab.com".user = "git";
-
-              #################
-              # Lepage Knives #
-              #################
-              vps-lepage-knives = {
-                host = "lk lepage-knives vps.lepage-knives.com";
-                hostname = "vps.lepage-knives.com";
-                user = "root";
-              };
-              borg-lk = {
-                hostname = "zh5001.rsync.net";
-                user = "zh5001";
-              };
-
-              ##############
-              # Cordineaux #
-              ##############
-              feroe.hostname = "10.10.10.23";
+            };
+            tank = {
+              hostname = "tank.glepage.com";
+              port = 222;
+            };
+            borg = {
+              hostname = "zh5071.rsync.net";
+              user = "zh5071";
             };
 
+            vps.hostname = "vps.glepage.com";
+
+            pixel = {
+              hostname = "10.10.10.3";
+              port = 2222;
+            };
+
+            "github.com".user = "git";
+            "gitlab.com".user = "git";
+
+            #################
+            # Lepage Knives #
+            #################
+            vps-lepage-knives = {
+              host = "lk lepage-knives vps.lepage-knives.com";
+              hostname = "vps.lepage-knives.com";
+              user = "root";
+            };
+            borg-lk = {
+              hostname = "zh5001.rsync.net";
+              user = "zh5001";
+            };
+
+            ##############
+            # Cordineaux #
+            ##############
+            feroe.hostname = "10.10.10.23";
+          }
+        );
       };
     };
 }
