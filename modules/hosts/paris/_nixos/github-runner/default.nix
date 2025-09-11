9d8a25d161c8198087a6cb6876bf39909129a68a
Gaetan Lepage <gaetan@glepage.com>
2025-09-11 23:17:37 +0200

    paris/github-runner: add LK self-hosted runner

--- modules/hosts/paris/_nixos/github-runner/default.nix
+++ modules/hosts/paris/_nixos/github-runner/default.nix
@@ -1,11 +1,22 @@
 { config, ... }:
 {
-  age.secrets.github-runner-token-file.rekeyFile = ./token-file.age;
+  age.secrets = {
+    github-runner-token-file.rekeyFile = ./token-file.age;
+    github-runner-lk-token-file.rekeyFile = ./token-file-lk.age;
+  };
 
-  services.github-runners.main = {
-    enable = true;
-    url = "https://github.com/GaetanLepage/nix-config";
+  services.github-runners = {
+    main = {
+      enable = true;
+      url = "https://github.com/GaetanLepage/nix-config";
 
-    tokenFile = config.age.secrets.github-runner-token-file.path;
+      tokenFile = config.age.secrets.github-runner-token-file.path;
+    };
+    lk = {
+      enable = true;
+      url = "https://github.com/LepageKnives";
+
+      tokenFile = config.age.secrets.github-runner-lk-token-file.path;
+    };
   };
 }
