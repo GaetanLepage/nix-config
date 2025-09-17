84cff33d40c22cf59beac5283dc62a16fba13bf7
Gaetan Lepage <gaetan@glepage.com>
2025-09-17 14:38:32 +0200

    home/ssh: remove unused aliases

--- modules/home/core/programs/ssh/aliases.nix
+++ modules/home/core/programs/ssh/aliases.nix
@@ -3,11 +3,6 @@
     home.shellAliases = {
       sb = "ssh builder";
       sba = "ssh builder -t zellij attach --create main";
-      sc = "ssh cuda";
-      sca = "ssh cuda -t zellij attach --create main";
-      sj = "ssh jrs";
-      sp = "ssh paris";
-      spa = "ssh paris -t zellij attach --create main";
     };
   };
 }
