1113c0e3fd50e98c27c84847ebed7fc5a42b1758
Gaetan Lepage <gaetan@glepage.com>
2025-09-17 13:32:28 +0200

    home/ssh/nix-builders: add the SSH key to HM agenix

--- modules/home/core/programs/ssh/nix-builders.nix
+++ modules/home/core/programs/ssh/nix-builders.nix
@@ -2,23 +2,23 @@
   flake.modules.homeManager.core =
     {
       lib,
-      osConfig ? null,
+      config,
       ...
     }:
     {
+      age.secrets.ssh-nix-community = {
+        rekeyFile = ../../../../nixos/dev/remote-builders/ssh-nix-community.age;
+        mode = "600";
+      };
+
       programs.ssh.matchBlocks =
         lib.mapAttrs
-          (
-            _: hostname:
-            {
-              inherit hostname;
-              user = "glepage";
-              setEnv.TERM = "xterm-256color";
-            }
-            // (lib.optionalAttrs (osConfig != null) {
-              identityFile = osConfig.age.secrets.ssh-nix-community.path;
-            })
-          )
+          (_: hostname: {
+            inherit hostname;
+            user = "glepage";
+            setEnv.TERM = "xterm-256color";
+            identityFile = config.age.secrets.ssh-nix-community.path;
+          })
           {
             arm = "aarch64-build-box.nix-community.org";
             darwin = "darwin-build-box.nix-community.org";
