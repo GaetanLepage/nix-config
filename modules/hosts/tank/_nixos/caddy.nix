a1e5672bc729cbb809c786a457af41a1ea396e14
Gaetan Lepage <gaetan@glepage.com>
2025-09-18 13:51:24 +0200

    hosts/tank/caddy: remove router reverse proxy

--- modules/hosts/tank/_nixos/caddy.nix
+++ modules/hosts/tank/_nixos/caddy.nix
@@ -7,12 +7,5 @@
 
   users.users.gaetan.extraGroups = [ "caddy" ];
 
-  services.caddy = {
-    enable = true;
-
-    reverseProxies."router.glepage.com" = {
-      localIp = "192.168.1.1";
-      vpn = true;
-    };
-  };
+  services.caddy.enable = true;
 }
