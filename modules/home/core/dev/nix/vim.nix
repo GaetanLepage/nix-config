fcc38375c5ded232417bdae02abfcc8dd9241794
Gaetan Lepage <gaetan@glepage.com>
2025-09-30 13:46:44 +0200

    home/dev/nix: fix formatting

--- modules/home/core/dev/nix/vim.nix
+++ modules/home/core/dev/nix/vim.nix
@@ -6,7 +6,7 @@
         lsp.servers.nil_ls = {
           enable = true;
           settings.settings.formatting.command = [
-              (lib.getExe pkgs.nixfmt)
+            (lib.getExe pkgs.nixfmt)
           ];
         };
 
