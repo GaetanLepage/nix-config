b0b90fe9e83d04b01355a1315d177637506bc4e2
Gaetan Lepage <gaetan@glepage.com>
2025-10-02 23:35:56 +0200

    hosts/builder: clear all nix extra-platforms

--- modules/hosts/builder/default.nix
+++ modules/hosts/builder/default.nix
@@ -3,6 +3,8 @@
     modules = [
       {
         age.rekey.hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJBLqR68XaTkJOZAq2Eb3rRugIHnALgtB64RuDQUSlJQ";
+
+        nix.settings.extra-platforms = [ ];
       }
 
       ./_gitub-ssh-auth.nix
