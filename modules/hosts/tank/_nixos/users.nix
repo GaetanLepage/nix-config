de8c6853e8b7d686a5cd770f6d47731d6da1e84d
Gaetan Lepage <gaetan@glepage.com>
2025-11-02 20:04:07 +0100

    hosts/tank: add francis user for macbook backups with borg

--- modules/hosts/tank/_nixos/users.nix
+++ modules/hosts/tank/_nixos/users.nix
@@ -18,6 +18,17 @@
         group = "lepage";
         isNormalUser = true;
       };
+
+      francis = {
+        isNormalUser = true;
+        group = "lepage";
+
+        openssh.authorizedKeys.keys = [
+          # SSH key for borg backups
+          # Backup destination: /tank/lepage_family/backup/fl_macbook/
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA4jMzH/oCEPsHYi0kph6/iDJV02V2NE1LLj9fPaD/rz francis@Host-003.lan"
+        ];
+      };
     };
 
     groups = {
