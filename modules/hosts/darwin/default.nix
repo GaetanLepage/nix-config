4e51a39ec1cefe01e1331dff91e7c2f871b0528c
Gaetan Lepage <gaetan@glepage.com>
2025-12-15 21:40:32 +0100

    hosts/darwin: set max-jobs to 8

--- modules/hosts/darwin/default.nix
+++ modules/hosts/darwin/default.nix
@@ -5,6 +5,7 @@
     modules = [
       {
         age.rekey.hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHVJ5H2tmG/IG3T1MJDc6GHrEhMHpCWKv+JTY2oxtZp7";
+        nix.settings.max-jobs = 8;
       }
     ];
   };
