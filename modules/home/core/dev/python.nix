db7a0416ee8c6b645eec7be64d76bbe0752fda3b
Gaetan Lepage <gaetan@glepage.com>
2025-12-20 00:41:41 +0100

    home/core/dev/python: switch from pylsp to ty

--- modules/home/core/dev/python.nix
+++ modules/home/core/dev/python.nix
@@ -47,28 +47,7 @@
           };
           lsp.servers = {
             ruff.enable = true;
-            pylsp = {
-              enable = true;
-              config.settings = {
-                plugins = {
-                  jedi_completion.fuzzy = true;
-
-                  pylsp_mypy.enabled = true;
-
-                  # We don't need those as ruff is already providing such features.
-                  autopep8.enabled = false;
-                  flake8.enabled = false;
-                  mccabe.enabled = false;
-                  preload.enabled = false;
-                  pycodestyle.enabled = false;
-                  pydocstyle.enabled = false;
-                  pyflakes.enabled = false;
-                  pylint.enabled = false;
-                  ruff.enabled = false;
-                  yapf.enabled = false;
-                };
-              };
-            };
+            ty.enable = true;
           };
         };
       };
