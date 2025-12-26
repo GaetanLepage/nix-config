8b01404c7a1249c3ab2a2f14659d907cf669bf30
Gaetan Lepage <gaetan@glepage.com>
2025-12-26 19:10:38 +0100

    hosts/darwin: set prompt hostname color to purple

--- modules/hosts/darwin/default.nix
+++ modules/hosts/darwin/default.nix
@@ -6,6 +6,7 @@
       {
         age.rekey.hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHVJ5H2tmG/IG3T1MJDc6GHrEhMHpCWKv+JTY2oxtZp7";
         nix.settings.max-jobs = 8;
+        programs.starship.settings.hostname.style = "bold purple";
       }
     ];
   };
