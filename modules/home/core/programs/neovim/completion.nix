2e9ccd0f3947d99b015c01f54302607439bc5dbe
Gaetan Lepage <gaetan@glepage.com>
2026-01-01 20:28:41 +0100

    home/tui/neovim: add blink-cmp-nixpkgs-maintainers

--- modules/home/core/programs/neovim/completion.nix
+++ modules/home/core/programs/neovim/completion.nix
@@ -1,62 +1,96 @@
 {
-  flake.modules.homeManager.core = {
-    programs.nixvim = {
-      opts.completeopt = [
-        "menu"
-        "menuone"
-        "noselect"
-      ];
+  flake.modules.homeManager.core =
+    { pkgs, lib, ... }:
+    {
+      programs.nixvim = {
+        opts.completeopt = [
+          "menu"
+          "menuone"
+          "noselect"
+        ];
 
-      plugins = {
-        luasnip.enable = true;
+        extraPlugins = [
+          (pkgs.vimUtils.buildVimPlugin {
+            pname = "blink-cmp-nixpkgs-maintainers";
+            version = "1";
+            src = pkgs.fetchFromGitHub {
+              owner = "GaetanLepage";
+              repo = "blink-cmp-nixpkgs-maintainers";
+              rev = "b572f807ca3b4b6f87c791b0d5ceaa81dbb00d70";
+              hash = "sha256-ollZI9Bd3eAKvzS84oIzToVruYbCERKKhZpIR+cBxBE=";
+            };
+            dependencies = [
+              pkgs.vimPlugins.blink-cmp
+            ];
+          })
+        ];
 
-        blink-cmp = {
-          enable = true;
+        plugins = {
+          luasnip.enable = true;
 
-          settings = {
-            cmdline.enabled = false;
+          cmp-nixpkgs-maintainers.enable = lib.mkForce false;
 
-            snippets.preset = "luasnip";
+          blink-cmp = {
+            enable = true;
 
-            completion = {
-              list.selection.preselect = false;
-              documentation = {
-                auto_show = true;
-                auto_show_delay_ms = 0;
+            settings = {
+              cmdline.enabled = false;
+
+              snippets.preset = "luasnip";
+
+              completion = {
+                list.selection.preselect = false;
+                documentation = {
+                  auto_show = true;
+                  auto_show_delay_ms = 0;
+                };
               };
-            };
 
-            keymap = {
-              "<Tab>" = [
-                "select_next"
-                "fallback"
-              ];
-              "<S-Tab>" = [
-                "select_prev"
-                "fallback"
-              ];
-              "<CR>" = [
-                "select_and_accept"
-                "fallback"
-              ];
-            };
+              keymap = {
+                "<Tab>" = [
+                  "select_next"
+                  "fallback"
+                ];
+                "<S-Tab>" = [
+                  "select_prev"
+                  "fallback"
+                ];
+                "<CR>" = [
+                  "select_and_accept"
+                  "fallback"
+                ];
+              };
 
-            sources = {
-              default = [
-                "lsp"
-                "path"
-                "snippets"
-                "buffer"
-                # "nixpkgs_maintainers" TODO
-              ];
-              providers = {
-                # Words from other open buffers can also be suggested.
-                buffer.opts.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
+              sources = {
+                default = [
+                  "lsp"
+                  "path"
+                  "snippets"
+                  "buffer"
+                ];
+                providers = {
+                  # Words from other open buffers can also be suggested.
+                  buffer.opts.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
+
+                  nixpkgs_maintainers = {
+                    name = "nixpkgs maintainers";
+                    module = "blink_cmp_nixpkgs_maintainers";
+                  };
+                };
+                per_filetype = {
+                  markdown = [
+                    "nixpkgs_maintainers"
+                    # TODO: https://github.com/nix-community/nixvim/pull/4137
+                    # {
+                    #   __unkeyed = "nixpkgs_maintainers";
+                    #   inherit_defaults = true;
+                    # }
+                  ];
+                };
               };
             };
           };
         };
       };
     };
-  };
 }
