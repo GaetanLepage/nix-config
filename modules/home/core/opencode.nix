bd15dbcc3bde374ea5212d6a1bef3bfd8987e9d7
Gaetan Lepage <gaetan@glepage.com>
2026-02-02 16:07:54 +0100

    home/core/opencode: use gpt 5.2 codex by default

--- modules/home/core/opencode.nix
+++ modules/home/core/opencode.nix
@@ -2,6 +2,10 @@
   flake.modules.homeManager.core = {
     programs.opencode = {
       enable = true;
+
+      settings = {
+        model = "openai/gpt-5.2-codex";
+      };
     };
 
     home.shellAliases.oc = "opencode";
