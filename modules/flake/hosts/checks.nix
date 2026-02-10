abaee661c4d11a1b2b3e85b32bdd6e543905a899
Gaetan Lepage <gaetan@glepage.com>
2026-02-10 14:14:59 +0100

    modules/flake/hosts: refactor

---
+++ modules/flake/hosts/checks.nix
@@ -0,0 +1,33 @@
+{
+  config,
+  ...
+}:
+{
+  perSystem =
+    {
+      pkgs,
+      lib,
+      system,
+      ...
+    }:
+    {
+      checks =
+        let
+          filterSystem = lib.filterAttrs (n: cfg: cfg.system == system);
+
+          extractChecks =
+            name: configs:
+            pkgs.symlinkJoin {
+              name = "${name}-checks";
+              paths = lib.pipe configs [
+                filterSystem
+                (lib.mapAttrsToList (_: cfg: cfg.package))
+              ];
+            };
+        in
+        lib.mapAttrs extractChecks {
+          nixos-hosts = config.nixosHosts;
+          home-hosts = config.homeHosts;
+        };
+    };
+}
---
+++ modules/flake/hosts/home.nix
@@ -0,0 +1,60 @@
+{
+  inputs,
+  lib,
+  config,
+  self,
+  ...
+}:
+let
+  inherit (lib) types mkOption;
+  baseHostModule = import ./_base-host-module.nix { inherit lib inputs; };
+in
+{
+  options = {
+    homeHosts =
+      let
+        hostType = types.submodule [
+          baseHostModule
+          (
+            { name, ... }:
+            {
+              modules = [
+                config.flake.modules.homeManager.core
+                (
+                  { pkgs, config, ... }:
+                  {
+                    nix.package = pkgs.nix;
+                    age.identityPaths = [ "${config.home.homeDirectory}/.ssh/agenix" ];
+                  }
+                )
+              ];
+              package = self.homeConfigurations.${name}.activationPackage;
+            }
+          )
+        ];
+      in
+      mkOption {
+        type = types.attrsOf hostType;
+        default = { };
+      };
+  };
+
+  config = {
+    flake.homeConfigurations =
+      let
+        mkHost =
+          configName: options:
+          inputs.home-manager.lib.homeManagerConfiguration {
+            extraSpecialArgs = {
+              inherit
+                inputs
+                configName
+                ;
+              nhSwitchCommand = "nh home switch --configuration ${configName}";
+            };
+            inherit (options) pkgs modules;
+          };
+      in
+      lib.mapAttrs mkHost config.homeHosts;
+  };
+}
