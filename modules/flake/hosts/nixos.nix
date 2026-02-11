eceebea98fc3325d4ad43b2c929de780ddefca01
Gaetan Lepage <gaetan@glepage.com>
2026-02-12 00:30:02 +0100

    flake/hosts/nixos: cleanup

--- modules/flake/hosts/nixos.nix
+++ modules/flake/hosts/nixos.nix
@@ -18,13 +18,13 @@ in
           (
             { name, ... }:
             {
+              system = lib.mkDefault "x86_64-linux";
+              package = self.nixosConfigurations.${name}.config.system.build.toplevel;
+
               modules = [
                 config.flake.modules.nixos.core
                 { networking.hostName = name; }
               ];
-              package = self.nixosConfigurations.${name}.config.system.build.toplevel;
-
-              system = lib.mkDefault "x86_64-linux";
             }
           )
         ];
