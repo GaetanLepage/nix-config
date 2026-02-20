bb5f653ef9f948aef6618c79426aad0c39561a82
Gaetan Lepage <gaetan@glepage.com>
2026-02-20 12:13:41 +0100

    home/core/nix: add a few settings

--- modules/home/core/nix.nix
+++ modules/home/core/nix.nix
@@ -4,7 +4,13 @@
       warn-dirty = false;
       experimental-features = "nix-command flakes";
 
+      max-jobs = "auto";
       use-xdg-base-directories = true;
+
+      # Prevent garbage collection from altering nix-shells managed by nix-direnv
+      # https://github.com/nix-community/nix-direnv#installation
+      keep-outputs = true;
+      keep-derivations = true;
     };
   };
 }
