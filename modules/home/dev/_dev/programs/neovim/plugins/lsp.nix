fd3a7c4e05398fc6951b4c7377abf9096ac51a6a
Gaetan Lepage <gaetan@glepage.com>
2026-04-26 00:18:02 +0200

    home/neovim/lsp-format: fix override

--- modules/home/dev/_dev/programs/neovim/plugins/lsp.nix
+++ modules/home/dev/_dev/programs/neovim/plugins/lsp.nix
@@ -40,8 +40,9 @@
       lsp-format = {
         enable = true;
         package = pkgs.vimPlugins.lsp-format-nvim.overrideAttrs (old: {
-          src = old.src.override {
+          src = pkgs.fetchFromGitHub {
             owner = "GaetanLepage";
+            repo = "lsp-format-nvim";
             rev = "new-api";
             hash = "sha256-OoubWk1om8we3MWA73PCGkax0nbOc+NxJ6pLq9KRfVE=";
           };
