2426a92e0668acd3658f465256f5cd216a1e6089
Gaetan Lepage <gaetan@glepage.com>
2026-06-22 10:57:01 +0200

    home/neovim/plugins: drop treesitter-refactor (archived plugin)

--- modules/home/dev/_dev/programs/neovim/plugins/treesitter.nix
+++ modules/home/dev/_dev/programs/neovim/plugins/treesitter.nix
@@ -12,22 +12,6 @@
       folding.enable = true;
     };
 
-    treesitter-refactor = {
-      # TODO: incompatible with treesitter-nvim's main branch
-      # Consider switching to nvim-treesitter-locals
-      # https://github.com/NixOS/nixpkgs/pull/476829
-      enable = false;
-
-      settings = {
-        highlight_definitions = {
-          enable = true;
-
-          # Set to false if you have an `updatetime` of ~100.
-          clear_on_cursor_move = false;
-        };
-      };
-    };
-
     hmts.enable = true;
   };
 }
