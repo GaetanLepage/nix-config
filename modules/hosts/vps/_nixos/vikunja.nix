ce6b6dcaa81d79128fedc84ebcfb0d38aa1949d1
Gaetan Lepage <gaetan@glepage.com>
2026-06-28 13:01:03 +0200

    hosts/vps/vikunja: allow unsecure pnpm

--- modules/hosts/vps/_nixos/vikunja.nix
+++ modules/hosts/vps/_nixos/vikunja.nix
@@ -3,6 +3,12 @@ let
   domain = "board.glepage.com";
 in
 {
+  # 2026-06-28: pnpm is marked as insecure
+  # https://github.com/NixOS/nixpkgs/issues/535579
+  nixpkgs.config.permittedInsecurePackages = [
+    "pnpm-10.29.2"
+  ];
+
   services = {
     caddy.reverseProxies.${domain} = {
       inherit (config.services.vikunja) port;
